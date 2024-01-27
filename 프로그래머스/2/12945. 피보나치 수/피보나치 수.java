a20c83c610260f6e26fe7690f7c4430a742ab9f3
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-27 18:43:17 +0900

    [level 2] Title: 피보나치 수, Time: 3.55 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    long[] dp;
+    
+    public long solution(int n) {
+        dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i <= n;i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567L;
+        }
+        
+        return dp[n];
+    }
+}
