301a1091b0f2151dd12d8003ae3f0ce70d737dc8
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-27 14:58:25 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -10,22 +10,14 @@ class Solution {
             if(index % 2 != 0){
                 // index 나누어 진다면 count++1;
                 if(n % index == 0){
-                    int div = n / index; 
-                    if(div - index / 2 > 0){
-                        count++;
-                        System.out.println(index);
-                    }
+                    count++;
                 }
             } 
             // 짝수개로 만들어야 한다면
             else {
                 // n-1 % index 0이라면 count ++;
                 if((n - index / 2) % index == 0){
-                    int div = (n - 1) / index; 
-                    if(div + 1 - index / 2 > 0){
-                        System.out.println(index);
-                        count++;
-                    }
+                    count++;
                 }
             }
             sum += index;  
