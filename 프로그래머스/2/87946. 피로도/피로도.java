04dc60743c119c61963fa88ad8270628d9ca3459
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-28 17:19:31 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -4,26 +4,26 @@ import java.util.*;
 class Solution {
     
     int maxCount = 0;
+    boolean[] isGo;
+    int[][] dun;
 
     public int solution(int k, int[][] dungeons) {
+        dun = dungeons;
         
-        boolean[] isGo = new boolean[dungeons.length];
-        Go(dungeons, k, 0, isGo);
+        isGo = new boolean[dungeons.length];
+        
+        Go(k, 0);
         
         return maxCount;
     }
     
-    public void Go(int[][] dun, int nowFiro, int moveCount, boolean[] isGo){
-        
+    public void Go(int nowFiro, int moveCount){
         for(int index = 0; index<isGo.length; index++){
-            if(
-                !isGo[index] &&
-                nowFiro >= dun[index][0] &&
-                nowFiro >= dun[index][1]
-            ){
-                boolean[] isGo2 = isGo.clone();
-                isGo2[index] = true;
-                Go(dun, nowFiro - dun[index][1],moveCount +1,  isGo2);
+            // 갈수 있으면
+            if(!isGo[index] && nowFiro >= dun[index][0]){
+                isGo[index] = true;
+                Go(nowFiro - dun[index][1],moveCount +1);
+                isGo[index] = false;
             }
             
             if(maxCount < moveCount){
