0af06f601c16bcb1fddc6f4baf04846157b0fede
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-02-01 19:49:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.44 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,8 @@
-def solution(participant, completion):
-    dic = {}
-    for com in completion:
-        dic[com] = dic.get(com, 0) + 1
-    
-    for par in participant:
+import collections
 
-        if par in dic:
-            if dic[par] >= 2:
-                dic[par] = dic[par] - 1
-            else:
-                dic.pop(par)
-        else:
-            return par
+def solution(participant, completion):
+    a = collections.Counter(participant) - collections.Counter(completion)
+    
+    return list(a).pop()
                 
             
