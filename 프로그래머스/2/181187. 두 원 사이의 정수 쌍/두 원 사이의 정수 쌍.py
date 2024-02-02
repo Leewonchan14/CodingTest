30aae5bc2befd5a5201ecbd0eeee30299b707e8c
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-02-02 11:58:14 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 368.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,15 +1,17 @@
 import math
 def solution(r1, r2):
     count = 0
+    
+    r1_sq = r1**2
+    r2_sq = r2**2
+    
     for x in range(1 , r2):
-        max_y = int((r2*r2 - x*x)**.5)
-        if (max_y + 1)**2 + x**2 == r2**2:
-            max_y +=1
+        max_y = int((r2_sq - x*x)**.5)
 
         min_y = 0
         if x < r1:
-            min_y = int((r1*r1-x*x)**.5)
-            if (min_y)**2 + x**2 == r1**2:
+            min_y = int((r1_sq-x*x)**.5)
+            if (min_y)**2 + x**2 == r1_sq:
                 min_y -=1
         
         count += max_y - min_y
