18707628e3957eb9e1b6ce1de1f3c57635ef544a
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-02-03 10:47:16 +0900

    [Bronze III] Title: 대지, Time: 2064 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -9,5 +9,6 @@ else:
         a,b = map(int , input().split())
         x.append(a)
         y.append(b)
-        
-    print((max(x) - min(x)) * (max(y) - min(y)))
+    x.sort()
+    y.sort()
+    print((x[-1] - x[0]) * (y[-1] - y[0]))
