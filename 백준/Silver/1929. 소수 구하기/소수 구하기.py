d07e8e14aa82849637ef500f874a181ca91e63d1
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-02-03 18:21:08 +0900

    [Silver III] Title: 소수 구하기, Time: 5644 ms, Memory: 115092 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,25 @@
+cache = {0: False, 1: False, 2: True}
+
+
+def is_prime(_n):
+    if _n < 1:
+        return False
+
+    if _n in cache:
+        return cache[_n]
+
+    index = 2
+    while True:
+        if _n % index == 0:
+            cache[_n] = False
+            return cache[_n]
+        index += 1
+        if index ** 2 > _n:
+            cache[_n] = True
+            return cache[_n]
+
+
+a,b = map(int, input().split())
+for i in range(a,b + 1):
+    if is_prime(i):
+        print(i)
