5e872fddcddd36e4bfdf18f58c854db046703722
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-02-27 17:03:37 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,22 +1,39 @@
-def solution(name):
-    # Step 1: Calculate the initial vertical cost to change each character
-    name_len = len(name)
-    vertical_cost = sum(min(ord(c) - ord('A'), ord('Z') - ord(c) + 1) for c in name)
-    
-    # Step 2: Initialize variables for horizontal movement cost
-    min_move = name_len - 1 # Worst case: moving right all the time
-    
-    # Step 3: Optimize horizontal movement
-    for idx in range(name_len):
-        next_idx = idx + 1
-        while next_idx < name_len and name[next_idx] == 'A':
-            next_idx += 1
-        
-        distance = min(idx, name_len - next_idx)
-        min_move = min(min_move, idx + name_len - next_idx + distance)
-        
-    return vertical_cost + min_move
+a_int = ord("A")
+z_int = ord("Z")
 
-# Example usage
-# name = "JAZ"
-# print(solution(name))
+
+def solution(name):
+    count = 0
+    index = 0
+    length = len(name)
+    while index < length:
+        s = name[index]
+
+        # 위 아래 판단후 더해주기
+        s_int = ord(s)
+        down_count = z_int - s_int + 1
+        up_count = s_int - a_int
+
+        count += min(down_count, up_count)
+
+        # 다음 index 정하기
+        temp_index = index + 1
+        # 마지막이 아니고 다음이 A 라면
+        while temp_index < length and name[temp_index] == "A":
+            temp_index += 1
+
+        # 마지막까지 A라면
+        if temp_index == length:
+            break
+
+        # 오른쪽으로 A가 안나올때 까지 끝까지 간 count
+        right_count = temp_index - index
+
+        # 왼쪽으로 갔을때 count
+        left_count = index + length - temp_index
+
+        count += min(left_count, right_count)
+
+        index = temp_index
+
+    return count
