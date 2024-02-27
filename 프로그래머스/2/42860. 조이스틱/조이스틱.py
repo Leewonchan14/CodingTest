0a3ae2f3ddc503e97e75b84372e9dbd514f1c7a4
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-02-27 21:05:00 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,39 +1,35 @@
-a_int = ord("A")
-z_int = ord("Z")
-
-
 def solution(name):
-    count = 0
-    index = 0
-    length = len(name)
-    while index < length:
-        s = name[index]
+    a_int, z_int = ord('A'), ord('Z')
 
-        # 위 아래 판단후 더해주기
-        s_int = ord(s)
-        down_count = z_int - s_int + 1
-        up_count = s_int - a_int
+    # 모든 상, 하 움직임 수를 더함
+    vertical_moves = sum(min(ord(c) - a_int, z_int - ord(c) + 1) for c in name)
 
-        count += min(down_count, up_count)
+    # 최악의 경우 len(name) - 1 만큼의 움직임이 필요함
+    min_horizontal_moves = len(name) - 1
 
-        # 다음 index 정하기
-        temp_index = index + 1
-        # 마지막이 아니고 다음이 A 라면
-        while temp_index < length and name[temp_index] == "A":
-            temp_index += 1
+    # 모든 문자에 대해 최소 움직임을 계산
+    for i, s in enumerate(name):
+        # A인 경우는 넘어감
+        if s == 'A':
+            continue
+        # A가 아닌 경우, 다음이 A가 아닌 문자가 나올때까지 이동
+        next_char_index = i + 1
+        while next_char_index < len(name) and name[next_char_index] == 'A':
+            next_char_index += 1
 
-        # 마지막까지 A라면
-        if temp_index == length:
-            break
+        # next_char_index = i + 1 부터 A가 아닌 문자가 나올때까지의 거리
 
-        # 오른쪽으로 A가 안나올때 까지 끝까지 간 count
-        right_count = temp_index - index
+        # 최소 움직임은 i번째 문자까지의 움직임과, 뒤로 돌아가는 움직임의 합
+        direct_move = i
+        # 뒤로 돌아가는 움직임은 len(name) - next_char_index
+        backward_move = len(name) - next_char_index
 
-        # 왼쪽으로 갔을때 count
-        left_count = index + length - temp_index
+        # 최소 움직임은 i번째 문자까지의 움직임과, 뒤로 돌아가는 움직임의 합, 그리고 둘 중 작은 값을 더한 값
+        min_horizontal_moves = min(min_horizontal_moves, direct_move + backward_move + min(direct_move, backward_move))
+        
+    # 모든 문자가 A인 경우는 0을 반환
+    if name == "A" * len(name):
+        return 0
 
-        count += min(left_count, right_count)
-
-        index = temp_index
-
-    return count
+    # 최종 움직임은 상, 하 움직임 수와 최소 가로 움직임 수의 합
+    return vertical_moves + min_horizontal_moves
