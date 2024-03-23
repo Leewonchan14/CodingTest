3286e5e4335debac0648d707ed3a4f735ac1d913
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-03-23 18:10:29 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 81.05 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,55 +1,65 @@
-import sys
-
-sys.setrecursionlimit(1000000)
-
-
-def dfs(row, col, isGo, land, now_count, l):
-    # 땅보다 큰곳에 왔다면 return
-    if col < 0 or col >= len(land[0]) \
-            or row < 0 or row >= len(land):
-        return now_count
-
-    # 이미 간 곳이라면 return
-    if isGo[row][col]:
-        return now_count
-
-    # 빈 땅이라면
-    if land[row][col] == 0:
-        return now_count
-
-    land[row][col] = l
-    # 먼저 석유 갯수 증가와 isGo 표시
-    now_count += 1
-    isGo[row][col] = True
-
-    # 오른쪽
-    now_count = dfs(row, col + 1, isGo, land, now_count, l)
-    # 왼쪽
-    now_count = dfs(row, col - 1, isGo, land, now_count, l)
-    # 아래
-    now_count = dfs(row + 1, col, isGo, land, now_count, l)
-    # 위
-    now_count = dfs(row - 1, col, isGo, land, now_count, l)
-
-    return now_count
+from collections import deque
 
 
 def solution(land):
-    isGo = [[False for _ in range(len(land[0]))] for _ in range(len(land))]
+    n = len(land)
+    m = len(land[0])
+
+    isGo = [[False for _ in range(m)] for _ in range(n)]
     oils = {0: 0}
-    result = 0
-    for row in range(len(land)):
-        for col in range(len(land[0])):
-            count = dfs(row, col, isGo, land, 0, len(oils) + 1)
-            if count != 0:
-                oils[len(oils) + 1] = count
 
-    for col in range(len(land[0])):
+    que = deque()
+
+    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for row in range(n):
+        for col in range(m):
+            # 갔다면 continue
+            if isGo[row][col]:
+                continue
+
+            # 빈땅이면 continue
+            if land[row][col] == 0:
+                continue
+
+            # 안가고 석유라면 Enque
+            count = 0
+            index = len(oils)
+
+            isGo[row][col] = True
+            land[row][col] = index
+            que.append((row, col))
+
+            while len(que) > 0:
+                _row, _col = que.popleft()
+                count += 1
+
+                for dx, dy in direction:
+                    n_row = _row + dx
+                    n_col = _col + dy
+
+                    # 땅을 넘으면
+                    if n_row >= n or n_row < 0 or n_col >= m or n_col < 0:
+                        continue
+
+                    # 빈땅이면 continue
+                    if land[n_row][n_col] == 0:
+                        continue
+
+                    # 안가면 Enque
+                    if not isGo[n_row][n_col]:
+                        land[n_row][n_col] = index
+                        isGo[n_row][n_col] = True
+                        que.append((n_row, n_col))
+
+            oils[index] = count
+
+    result = []
+
+    for col in range(m):
         total = set()
-        for row in range(len(land)):
+        for row in range(n):
             total.add(land[row][col])
+        result.append(sum(map(lambda x: oils[x], total)))
 
-        result = max(sum(map(lambda x: oils[x], total)), result)
-
-    return result
-
+    return max(result)
