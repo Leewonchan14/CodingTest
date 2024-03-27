8a33d1547d551ac6c8c381fd260c15c74fee687a
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-03-27 12:13:53 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,14 @@
+def solution(number, k):
+    stk = []
+    for num in number:
+        while stk and stk[-1] < num and k > 0:
+            stk.pop()
+            k -= 1
+        stk.append(num)
+
+    if k > 0:
+        stk = stk[:-k]
+    return str(int("".join(stk)))
+
+
+# print(solution("333222111", 2))
