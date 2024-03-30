0461a1a094883fb6fef1c60b646f64c64aeac476
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-03-30 13:03:08 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,61 @@
+"""
+1. 아이디어
+    - 정렬이 되어있는 deque을 이용해 insert, delete 연산을 하며 마지막에 첫원소, 마지막 원소를 반환한다.
+    - 원소를 집어넣을때는 이분탐색을 이용한다.
+
+2. 시간 복잡도
+    - insert : log(n)
+    - 최댓값, 최소값 delete : 1
+    1000000 의 경우의 수를 log(n)번
+    10 ** 6 == 2 ** 20
+    최악의수 => n * log(n)
+    => 1000000 * 20 = 2천만 == 가능
+
+3. 자료 구조
+    que : Deque
+"""
+
+from collections import deque
+
+
+def index(que, start, end, value):
+    if len(que) == 0:
+        return 0
+
+    if start == end:
+        return start
+
+    mid = (start + end) // 2
+
+    if value < que[mid]:
+        return index(que, start, mid, value)
+    elif value > que[mid]:
+        return index(que, mid + 1, end, value)
+    else:
+        return mid
+
+
+def solution(operations):
+    que = deque()
+    
+    for item in operations:
+        oper, value = item.split()
+        value = int(value)
+        
+        if oper == "I":
+            idx = index(que, 0, len(que), value)
+            que.insert(idx, value)
+            
+        elif oper == "D":
+            if que:
+                if value == -1:
+                    que.popleft()
+                elif value == 1:
+                    que.pop()
+        
+    if que:
+        return [que[-1], que[0]]
+    else:
+        return [0, 0]
+                
+
