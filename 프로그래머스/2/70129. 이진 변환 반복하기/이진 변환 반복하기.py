e5cf36d3a088f4697eadae7c8d1ee3847e74f8ac
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-01 11:02:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.18 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -2,8 +2,15 @@ def solution(s):
     count = 0
     zero_count = 0
     while s != "1":
-        zero_count += s.count("0")
-        s = s.replace("0", "")
-        s = bin(len(s))[2:]
         count += 1
+        non_zero = list(filter(lambda x : x != "0", list(s)))
+        length = len(non_zero)
+        pre_length = len(s)
+        
+        zero_count += (pre_length - length)
+        
+        bin_length = bin(length)[2:]
+        s = bin_length
     return [count, zero_count]
+    
+    
