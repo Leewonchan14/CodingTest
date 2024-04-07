a15851004068b681a982406f1ec0df1a5aab9f65
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-07 23:14:38 +0900

    [level 2] Title: 롤케이크 자르기, Time: 861.93 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,31 +1,28 @@
 def solution(topping):
-    length = len(topping)
+    a_dic = {}
+    a_len = 0
+    b_dic = {}
+    for t in topping:
+        b_dic[t] = b_dic.get(t, 0) + 1
+        
+    b_len = len(b_dic.keys())
     
-    left_dic = {}
-    right_dic = {}
-    
-    answer = 0
-    
-    for i in range(length):
-        topping_item = topping[i]
+    index = 0
+    ls = []
+    while index < len(topping):
+        if topping[index] not in a_dic:
+            a_len += 1
+        a_dic[topping[index]] = a_dic.get(topping[index] , 0) + 1
         
-        right_dic[topping_item] = right_dic.get(topping_item, 0) + 1
-        
-    for i in range(length):
-        
-        topping_item = topping[i]
-        
-        left_dic[topping_item] = left_dic.get(topping_item, 0) + 1
-        
-        
-        right_item = right_dic.get(topping_item)
-        
-        if right_item - 1 == 0:
-            right_dic.pop(topping_item)
-        else :
-            right_dic[topping_item] = right_item - 1
-        
-        if len(left_dic.keys()) == len(right_dic.keys()):
-            answer += 1
+        pop = b_dic.pop(topping[index])
+        if pop - 1 == 0:
+            b_len -= 1
+        else:
+            b_dic[topping[index]] = pop - 1
             
-    return answer
+        if a_len == b_len:
+            ls.append(index)
+        
+        index += 1
+        
+    return len(ls)
