1af3a5fb6a2d0f0080f73628674ba5b152fbb823
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-07 20:45:30 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 1468.08 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 정사각형 찾기 - 12905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905) 
 
 ### 성능 요약
 
-메모리: 39.4 MB, 시간: 620.17 ms
+메모리: 30.8 MB, 시간: 1468.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 12:20:18
+2024년 04월 07일 20:45:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,18 +1,20 @@
 def solution(board):
-    dp = [[0 for _ in range(len(board[0]) + 1)] for _ in range(len(board) + 1)]
-    _max = 0
-    
-    for row in range(1, len(board) + 1):
-        for col in range(1, len(board[0]) + 1):
-            if board[row-1][col-1] == 0:
-                continue
-            
-            left = dp[row][col - 1]
-            up = dp[row - 1][col]
-            cross = dp[row - 1][col - 1]
-            
-            dp[row][col] = min([left, up, cross]) + board[row-1][col-1]
-            
-            if dp[row][col] > _max :
-                _max = dp[row][col]
-    return _max**2
+    dr = [-1 , 0 , -1]
+    dc = [0, -1, -1]
+    rows = len(board)
+    columns = len(board[0])
+    maxv = 0
+    for r in range(rows):
+        for c in range(columns):
+            if board[r][c] == 1:
+                ls = []
+                for i in range(3):
+                    if 0<= (r + dr[i]) < rows and 0 <= (c + dc[i]) < columns:
+                        ls.append(board[r + dr[i]][c + dc[i]])
+                    else:
+                        ls.append(0)
+                board[r][c] = min(ls) + 1
+                maxv = max(maxv , board[r][c])
+                
+    return maxv ** 2
+                
