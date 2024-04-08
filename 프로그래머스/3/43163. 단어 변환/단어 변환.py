b5e6e961931263381d18035a21f3cc443dbfe5e5
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-08 13:24:47 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,51 +1,61 @@
-import itertools
-
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
 from collections import deque
 
-def bfs(visited, linked, begin, target):
-    que = deque()
-    count = 0
-    que.append((begin, count))
-    while que:
-        item, count = que.popleft()
-        if item == target:
-            return count
-        
-        for link in linked[item]:
-            if not visited[link]:
-                visited[link] = True
-                que.append((link, count + 1))
-    
-    return 0
-    
 
-def is_one_gap(a,b):
-    index = 0
-    count = 0
-    while index < len(a):
-        if a[index] != b[index]:
-            count += 1
-        index += 1
-        
-    return count == 1
+def get_adjacent(current, words):
+    for word in words:
+        if len(current) != len(word):
+            continue
+
+        count = 0
+        for c, w in zip(current, word):
+            if c != w:
+                count += 1
+
+        if count == 1:
+            yield word
+
 
 def solution(begin, target, words):
-    linked = {begin : []}
-    visited = {}
-    
-    for w in words:
-        visited[w] = False
-        if is_one_gap(begin, w):
-            linked[begin].append(w)
-    
-    for a,b in itertools.combinations(words, 2):
-        if is_one_gap(a,b):
-            linked[a] = linked.get(a, [])
-            linked[a].append(b)
-            
-            linked[b] = linked.get(b, [])
-            linked[b].append(a)
-            
-    return bfs(visited, linked, begin, target)
-            
-    
+    dist = {begin: 0}
+    queue = deque([begin])
+
+    while queue:
+        current = queue.popleft()
+
+        for next_word in get_adjacent(current, words):
+            if next_word not in dist:
+                dist[next_word] = dist[current] + 1
+                queue.append(next_word)
+
+    return dist.get(target, 0)
