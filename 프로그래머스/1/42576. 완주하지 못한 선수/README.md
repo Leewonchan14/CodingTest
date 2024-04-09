089d3b89935541b3cad74b82cc369e7a39de78ed
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-10 00:28:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.19 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 38.9 MB, 시간: 66.44 ms
+메모리: 33.9 MB, 시간: 56.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 19:49:23
+2024년 04월 10일 00:28:33
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,17 @@
-import collections
-
 def solution(participant, completion):
-    a = collections.Counter(participant) - collections.Counter(completion)
+    dic = {}
+    for p in participant:
+        dic[p] = dic.get(p, 0) + 1
+        
+    for c in completion:
+        if c in dic and dic[c] > 1:
+            dic[c] -= 1
+        elif c in dic and dic[c] == 1:
+            del dic[c]
+            
+    return list(dic.keys())[0]
+            
+            
     
-    return list(a).pop()
                 
             
