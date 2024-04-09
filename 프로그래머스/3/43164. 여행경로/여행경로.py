6278def6262d434a4401ebe86f07880b3a30db25
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-09 19:38:30 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,34 +1,29 @@
-from collections import deque
-
 rs = []
+ls = ["ICN"]
+go = {}
+cnt = [0]
 
-
-def dfs(linked, start, ls, count):
-    if len(ls) == count + 1:
+def track(num):
+    if num == cnt[0]:
         rs.append(ls[:])
         return
-
-    for i in range(len(linked[start])):
-        if not linked[start][i][1]:
-            linked[start][i][1] = True
-            ls.append(linked[start][i][0])
-            dfs(linked, linked[start][i][0], ls, count)
-            linked[start][i][1] = False
+    
+    for g in go.get(ls[-1],[]):
+        if not g[1]:
+            g[1] = True
+            ls.append(g[0])
+            track(num + 1)
             ls.pop()
-
+            g[1] = False
 
 def solution(tickets):
-    linked = {}
-    count = 0
-    for a, b in tickets:
-        linked[a] = linked.get(a, [])
-        linked[a].append([b, False])
-        count += 1
-
-        linked[b] = linked.get(b, [])
-
-    dfs(linked, "ICN", ["ICN"], count)
-
-    rs.sort(key=lambda x: "".join(x))
-
+    for a,b in tickets:
+        cnt[0] += 1
+        go[a] = go.get(a, [])
+        go[a].append([b, False])
+        
+    track(0)
+    
+    rs.sort(key=lambda x : "".join(x))
+    
     return rs[0]
