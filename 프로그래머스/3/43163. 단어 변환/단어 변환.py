c24cec1f142b98e863b1548d7012b2649396124e
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-09 20:09:51 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,61 +1,23 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
 from collections import deque
 
+def gap3(a,b):
+    return sum([1 if a[i] != b[i] else 0 for i in range(len(a))]) == 1
 
-def get_adjacent(current, words):
-    for word in words:
-        if len(current) != len(word):
-            continue
-
-        count = 0
-        for c, w in zip(current, word):
-            if c != w:
-                count += 1
-
-        if count == 1:
-            yield word
-
+def bfs(begin, target, words):
+    que = deque()
+    que.append((begin, 0))
+    while que:
+        x, cnt = que.popleft()
+        if x == target:
+            return cnt
+        
+        if cnt >= len(words):
+            return 0
+        
+        for w in words:
+            if gap3(w, x):
+                que.append((w, cnt + 1))
+    return 0
 
 def solution(begin, target, words):
-    dist = {begin: 0}
-    queue = deque([begin])
-
-    while queue:
-        current = queue.popleft()
-
-        for next_word in get_adjacent(current, words):
-            if next_word not in dist:
-                dist[next_word] = dist[current] + 1
-                queue.append(next_word)
-
-    return dist.get(target, 0)
+    return bfs(begin, target, words)
