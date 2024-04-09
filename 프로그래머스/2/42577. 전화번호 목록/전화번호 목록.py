c2d1938609116390545785c7793c2ff395f4dad2
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-10 00:43:35 +0900

    [level 2] Title: 전화번호 목록, Time: 398.93 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    dic = set()
+    phone_book.sort()
+    for p in phone_book:
+        for i in range(1, len(p) + 1):
+            if p[:i] in dic:
+                return False
+        dic.add(p)
+    return True
+
+print(solution( ["010111", "010"] ))
