eceac3c1cf607dd96220b71fe1c04eefb9b17b69
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-15 16:07:59 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 60.90 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,39 +1,35 @@
 def solution(gems):
-    count = {}
-    deq = {}
+    dic = {}
     for i in gems:
-        count[i] = count.get(i, 0) + 1
-    
-    target = len(count.keys())
+        dic[i] = dic.get(i, 0) + 1
+        
+    target = len(dic.keys())
     
     left = 0
     right = 0
-        
+    
+    deq = {}
+    
     ls = []
-        
-    while right < len(gems):
+    
+    while True:
         while right < len(gems) and len(deq.keys()) != target:
             deq[gems[right]] = deq.get(gems[right], 0) + 1
             right += 1
-                
-        if len(deq.keys()) == target:
-            while deq[gems[left]] >= 2:
-                deq[gems[left]] -= 1 
-                left += 1
-
-            ls.append([left + 1, right])
             
-            deq.pop(gems[left])
+        if len(deq.keys()) != target:
+            break
+            
+        while deq[gems[left]] >= 2:
+            deq[gems[left]] -= 1
             left += 1
-        
-    ls.sort(key=lambda x : (x[1] - x[0], x[0]))
-    
-    print(ls)
+
+        ls.append([left + 1, right])
+        deq.pop(gems[left])
+        left += 1
+            
+    ls.sort(key=lambda x: (x[1] - x[0], x[0]))
     
     return ls[0]
-        
-        
-    
-    
-    
+                
     
