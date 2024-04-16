556c1240ab0b7fdebcd69ecff72342cf1b9d810e
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-16 18:19:08 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 684.97 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,24 +1,16 @@
-def solve(li):
-    length = len(li)
+def solve(sequence):
+    length = len(sequence)
     dp = [0 for _ in range(length)]
-    dp[0] = li[0]
-    for i in range(1,length):
-        if dp[i - 1] >= 0:
-            dp[i] = dp[i - 1] + li[i]
-        elif dp[i - 1] < 0:
-            dp[i] = li[i]
-            
+    dp[0] = sequence[0]
+    for i in range(1, length):
+        dp[i] = max(dp[i - 1] + sequence[i], sequence[i])
+        
     dp.sort()
     return dp[-1]
-        
-        
-
-
+    
 
 def solution(sequence):
     length = len(sequence)
     a = [(-1 if i % 2 == 0 else 1) * sequence[i] for i in range(length)]
     b = [(-1 if i % 2 != 0 else 1) * sequence[i] for i in range(length)]
     return max(solve(a), solve(b))
-
-
