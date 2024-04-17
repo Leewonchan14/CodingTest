896de4d92e2353771173754a3772e4190382f84f
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-17 09:33:52 +0900

    [level 3] Title: 징검다리 건너기, Time: 177.79 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,29 +1,38 @@
-def isCan(stones, k, n):
-    n = n - 1
+def CanBe(mid, stones, length, k):
+    mid = mid - 1
     cnt = 0
-    index = 0
     for s in stones:
-        if s - n > 0:
+        if s - mid > 0:
             cnt = 0
             continue
+        
         cnt += 1
         if cnt >= k:
             return False
         
     return True
-
-
+    
+            
+        
+    
 def solution(stones, k):
     left = 0
     right = max(stones)
-    answer = 0
-
+    length = len(stones)
+    
+    result = 0
+    
     while left <= right:
         mid = (left + right) // 2
-        if isCan(stones, k, mid):
-            answer = max(answer, mid)
+        if CanBe(mid, stones, length, k):
+            result = max(result, mid)
             left = mid + 1
+            
         else:
             right = mid - 1
-
-    return answer
+            
+    return result
+            
+    
+    
+    
