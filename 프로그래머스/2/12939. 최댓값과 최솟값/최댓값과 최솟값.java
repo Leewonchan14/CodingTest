e777defd30d7f465c735b5b1c8af88b0b828b2a8
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-21 14:34:04 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.98 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,9 +1,19 @@
-import java.util.stream.*;
-import java.util.*;
 class Solution {
-    public String solution(String s) {
-        String[] arr = s.split(" ");
-        int[] arr2 = Arrays.stream(arr).mapToInt(Integer::parseInt).sorted().toArray();
-        return arr2[0] + " " + arr2[arr.length-1];
+    public String solution(String st) {
+        String[] strs = st.split(" ");
+        int max = Integer.parseInt(strs[0]);
+        int min = Integer.parseInt(strs[0]);
+        for(String s : strs){
+            int target = Integer.parseInt(s);
+            if (target > max){
+                max = target;
+            }
+            if (target < min){
+                min = target;
+            }
+        }
+        
+        return min + " " + max;
+        
     }
 }
