4bef52f73c4cdb6801f745492623bcb9b7a6d7ab
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-21 15:34:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.72 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,110 +1,34 @@
-import java.util.stream.*;
 import java.util.*;
 class Solution {
-    // 최대 행
-    private int maxRow;
-    
-    // 최대 열
-    private int maxColumn;
-    
-    // Create Queue
-    private Queue<Node> que = new LinkedList<>();
-    
     public int solution(int[][] maps) {
-
-        maxRow = maps.length - 1;
-        maxColumn = maps[0].length - 1;
-
-        // 처음 노드 갔다고 표시하고 Queue에 넣기
-        maps[0][0] = 0;
-        que.add(new Node(0,0, 1));
-        // 정답을 return 할 변수
-        int moveCount = 1;
-
-        // Queue가 비어있지 않으면
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        int[] dy = {0 , -1, 0 , 1};
+        int[] dx = {1, 0 , -1 , 0};
+        LinkedList<int[]> que = new LinkedList();
+        int[] yx = {0,0,0};
+        que.addLast(yx);
         while(!que.isEmpty()){
-
-            // Queue에서 하나 뽑기
-            Node node = que.poll();
-
-            // MoveCount 가져오기
-            moveCount = node.getMoveCount();
-
-            // 만약 목적지에 도착 했다면
-            if (node.getRow() == maxRow && node.getCol() == maxColumn){
-                return moveCount;
+            int[] dot = que.pollFirst();
+            if (dot[0] == maps.length - 1 && dot[1] == maps[0].length - 1){
+                return dot[2] + 1;
             }
-
-            // 모든 방향 check
-            for (Direction dir : Direction.values()) {
-
-                int nextRow = node.getRow() + dir.getRow();
-                int nextCol = node.getCol() + dir.getCol();
-
-                // 만약 갈수 있으면
-                if(check(nextRow, nextCol, maps)){
-                    // 갔다고 체크 후
-                    maps[nextRow][nextCol] = 0;
-                    // Enqueue
-                    que.add(new Node(nextRow, nextCol, moveCount + 1));
+            
+            for(int i = 0; i < 4; i++){
+                int ny = dot[0] + dy[i];
+                int nx = dot[1] + dx[i];
+                if(0<= ny && ny < maps.length &&
+                   0 <= nx && nx < maps[0].length &&
+                   maps[ny][nx] == 1 &&
+                   !visited[ny][nx]){
+                    visited[ny][nx] = true;
+                    que.addLast(new int[] {ny,nx, dot[2] + 1});
                 }
+                    
+                
             }
         }
+        
+        
         return -1;
-    }
-
-    public boolean check(int row, int col, int[][] maps) {
-        // 갈수있는지 확인
-        if (
-                0 <= row && row <= this.maxRow &&
-                        0 <= col && col <= this.maxColumn
-        ) {
-            // 안가봤거나 뚫려있으면
-            return maps[row][col] == 1;
-        }
-        return false;
-    }
-    
-    public class Node {
-        private final int row;
-        private final int col;
-        private final int moveCount;
-
-        public Node(int row, int col, int moveCount) {
-            this.row = row;
-            this.col = col;
-            this.moveCount = moveCount;
-        }
-
-        public int getRow() {
-            return row;
-        }
-
-        public int getCol() {
-            return col;
-        }
-
-        public int getMoveCount() {
-            return moveCount;
-        }
-    }
-
-    public enum Direction {
-        UP(-1, 0), DOWN(1, 0), LEFT(0, -1), RIGHT(0, 1);
-        private int row;
-        private int col;
-
-        Direction(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        public int getRow() {
-            return row;
-        }
-
-        public int getCol() {
-            return col;
-        }
     }
 }
