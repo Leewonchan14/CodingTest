2cd7a7fcd3743c4419a15153afb795520a334de6
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-21 17:16:42 +0900

    [level 2] Title: 구명보트, Time: 5.15 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,111 +1,41 @@
+import java.util.*;
+
 class Solution {
+    int[] counts = new int[300];
     public int solution(int[] people, int limit) {
-        People ple = new People(people);
+        int max = 0;
+        int min = 300;
+        for(int weight : people){
+            counts[weight]++;
+            if (weight > max) max = weight;
+            if (weight < min) min = weight;
+        }
         
-        int answer = 0;
-        
-        int smallWeight = 0;
-        
+        int[] boat = new int[2];
+        int cnt = 0;
         while(true){
-            // 가장 큰 몸무게를 뽑아 온다.
-            int bigWeight = ple.pollBigWeight();
-            // 만약 0 이 나왔다면 break;
-            if(bigWeight == 0) break;
-            
-            // 가장 작은 몸무게를 가져온다.
-            smallWeight = ple.peekSmallWeight();
-            
-            // 만약 큰무게와 작은 무게가 합쳐서 limit보다 작거나 같다면 태워 보낸다.
-            if(bigWeight + smallWeight <= limit){
-                answer++;
-                ple.pollSmallWeight();
-                // 다음 사람 조사
-                continue;
-            }
-            
-            // 만약 limit보다 크다면 큰사람만 태워 보낸다.
-            answer++;
-        }
-    
-        if(ple.pollSmallWeight() > 0){
-            answer++;
-        }
-        
-        return answer;
-    }
-    
-    class People {
-        // 모든 무게의 갯수를 담을 배열 선언
-        private int[] weightCountArray = new int[241];
-        
-        private int smallWeight = 0;
-        private int bigWeight = 240;
-        
-        People(int[] people){
-            // 무게를 인덱스로 갯수를 count
-            for(int index = 0; index<people.length; index++){
-                int weight = people[index];
-                weightCountArray[weight] += 1;
-            }
-        }
-        
-        //가장 큰 무게를 가진 사람의 무게를 peek 하는 메서드
-        public int peekBigWeight(){
-            int bigWeightCount = weightCountArray[bigWeight];
-            
-            // count가 존재 할때까지 
-            while(bigWeightCount == 0){
+            cnt += 1;
                 
-                bigWeight--;
-                // 만약 작은 몸무게 보다 작아졌다면 0을 리턴
-                if(bigWeight < smallWeight){
-                    return 0;
-                }
-                bigWeightCount = weightCountArray[bigWeight];
+            if (max + min <= limit && !(min == max && counts[min] <= 1)){
+                counts[max] -= 1;
+                counts[min] -= 1;
+            } else {
+                counts[max] -= 1;
             }
             
-            return bigWeight;
-        }
-        
-        //가장 큰 무게를 가진 사람의 무게를 pop하는 메서드
-        public int pollBigWeight(){
-            // 가장큰 몸무게 가져오기
-            bigWeight = peekBigWeight();
-            
-            // 1제거
-            weightCountArray[bigWeight] -= 1;
-            
-            return bigWeight;
-        }
-        
-        //가장 작은 무게를 가진 사람의 무게를 pop하는 메서드
-        public int pollSmallWeight(){
-            int smallWeight = peekSmallWeight();
-            
-            // 1제거
-            weightCountArray[smallWeight] -= 1;
-            
-            return smallWeight;
-        }
-        
-        //가장 작은 무게를 가진 사람의 무게를 peek 하는 메서드
-        public int peekSmallWeight(){
-            int smallWeightCount = weightCountArray[smallWeight];
-            
-            // count가 존재 할때까지 
-            while(smallWeightCount == 0){
-                
-                smallWeight++;
-                // 만약 큰 몸무게 보다 커졌다면 0을 리턴
-                if(bigWeight < smallWeight){
-                    return 0;
-                }
-                
-                smallWeightCount = weightCountArray[smallWeight];
+            while(0 <= max && counts[max] == 0){
+                max -= 1;
             }
+            while(min < 300 && counts[min] == 0){
+                min += 1;
+            }
+            if (max == -1 || min == 300) break;
             
-            return smallWeight;
+            if(min == max && counts[max] == 0){
+                break;
+            }
         }
         
+        return cnt;
     }
 }
