6deabecc8c173a16839dbf5cfeb466a39f1ee09f
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-21 14:50:17 +0900

    [level 2] Title: 피보나치 수, Time: 2.30 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
 
 ### 성능 요약
 
-메모리: 13.9 MB, 시간: 16.08 ms
+메모리: 78.4 MB, 시간: 2.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 11:17:35
+2024년 04월 21일 14:50:16
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,11 @@
 class Solution {
-    
-    long[] dp;
-    
-    public long solution(int n) {
-        dp = new long[n + 1];
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
-        for(int i = 2;i <= n;i++){
-            dp[i] = (dp[i-1] + dp[i-2]) % 1234567L;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
         
         return dp[n];
