1a75a118c134e77046e9fcd91333074de146161c
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-23 22:11:06 +0900

    [level 2] Title: 하노이의 탑, Time: 7.62 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,36 +1,20 @@
 import java.util.*;
-
+import java.util.stream.*;
 class Solution {
-    private static List<int[]> list = new ArrayList();
-    
+    List<int[]> result = new ArrayList();
     public int[][] solution(int n) {
-        // 옮긴다.
-        move(1,3,2,n);
-        
-        int[][] answer = new int[list.size()][2];
-        
-        // 컬렉션을 배열으로
-        for(int i = 0;i<list.size();i++){
-            answer[i] = list.get(i);
-        }
-        return answer;
+        move(1, 3, 2, n);
+        return result.stream().toArray(int[][]::new);
     }
-    public static void move(int start, int end, int other,int num){
-        //만약 옮기는 원판이 한개인 경우 그대로 옮기고 return
-        if(num<=1){
-            int[] item = new int[]{start, end};
-            list.add(item);
-            return;
-        } 
+    
+    public void move(int start, int end, int other, int cnt){
+        if (cnt == 1){
+            result.add(new int[] { start, end });
+            return ;
+        }
         
-        // 맨아래 원판을 제외한 위 원판을 모두 other로 옮긴후
-        move(start, other, end, num-1);
-        // 맨아래 원판을 end로
+        move(start, other, end, cnt - 1);
         move(start, end, other, 1);
-        // other로 옮긴 원판들 end로 이동
-        move(other, end, start, num-1);
-            
-        return;
-        
+        move(other, end, start, cnt - 1);
     }
 }
