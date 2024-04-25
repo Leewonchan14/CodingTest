e8065dcf6caf4cf13400eaddf157db9de55d7a74
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-25 12:26:42 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 196 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -3,12 +3,12 @@ import java.util.stream.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        LinkedList<String> list = new LinkedList();
         String str = sc.next();
-        for (String s : str.split("")){
-            list.addFirst(s);
+        char[] chars = new char[str.length()];
+        for (int i = 0; i < str.length(); i ++){
+            chars[i] = str.charAt(str.length() - i - 1);
         }
-        String reverse = String.join("", list);
+        String reverse = String.valueOf(chars);
         if(str.equals(reverse)){
             System.out.println(1);
         } else{
