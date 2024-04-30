8f9a915f5b95c7cb1cb17a1bdb9787abe6a36e45
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-30 13:12:32 +0900

    [level 2] Title: 배달, Time: 3.62 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 0.41 ms
+메모리: 75.2 MB, 시간: 3.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 12:41:36
+2024년 04월 30일 13:12:30
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,61 +1,59 @@
 import java.util.*;
 import java.util.stream.*;
 
-class Go{
-    public int weight;
-    public int n;
-    
-    public Go(int weight, int n){
-        this.weight = weight;
-        this.n = n;
-    }
-    
-    public String toString(){
-        return  "weight : " + weight + ", n : " + n;
-    }
-}
-
 class Solution {
-    
-    List<Go>[] dir;
-    int[] weights;
-    
-    public int daikstra(int K){
-        PriorityQueue<Go> que = new PriorityQueue<>((Go g1, Go g2) -> g1.weight - g2.weight);
+    public class Node{
+        public int weight = 2000000000;
+        public List<Edge> edges = new ArrayList<>();
         
-        que.add(new Go(0, 1));
+        public String toString(){
+            return "weight : " + weight;
+        }
+    }
+    
+    public class Edge{
+        public Node a;
+        public Node b;
+        public int weight;
+        
+        public Edge(Node a, Node b, int weight){
+            this.a = a;
+            this.b = b;
+            this.weight = weight;
+        }
+    }
+    
+    public int solution(int N, int[][] road, int K) {
+        Node[] nodes = new Node[N + 1];
+        for(int i = 0 ; i < N + 1 ; i++){
+            nodes[i] = new Node();
+        }
+        
+        for(int[] r : road){
+            nodes[r[0]].edges.add(new Edge(nodes[r[0]], nodes[r[1]], r[2]));
+            nodes[r[1]].edges.add(new Edge(nodes[r[1]], nodes[r[0]], r[2]));
+        }
+        
+        PriorityQueue<Edge> que = new PriorityQueue<>((Edge e1, Edge e2)-> e1.weight - e2.weight);
+        
+        Node now = nodes[1];
+        
+        now.weight = 0;
+        
+        for(Edge e : now.edges){
+            que.add(e);
+        }
         
         while(!que.isEmpty()){
-            Go g = que.poll();
-            for(Go ng : dir[g.n]){
-                if(ng.weight + g.weight < weights[ng.n]){
-                    weights[ng.n] = ng.weight + g.weight;
-                    que.add(new Go(weights[ng.n], ng.n));
-                }
+            Edge e = que.poll();
+            if(e.a.weight + e.weight >= e.b.weight) continue;
+            
+            e.b.weight = e.a.weight + e.weight;
+            for(Edge e2 : e.b.edges){
+                que.add(e2);
             }
         }
         
-        return (int)Arrays.stream(weights).filter(i->i <= K).count();
-    }
-    
-    
-    public int solution(int N, int[][] road, int K) {
-        dir = new List[N + 1];
-        for (int i = 0; i < N + 1; i++){
-            dir[i] = new ArrayList<Go>();
-        }
-        
-        weights = new int[N + 1];
-        for(int i = 0 ; i < N + 1; i++){
-            weights[i] = 2000000000;
-        }
-        weights[1] = 0;
-        
-        for(int[] r : road){
-            dir[r[0]].add(new Go(r[2], r[1]));
-            dir[r[1]].add(new Go(r[2], r[0]));
-        }
-
-        return daikstra(K);
+        return (int)Arrays.stream(nodes).filter(n -> n.weight <= K).count();
     }
 }
