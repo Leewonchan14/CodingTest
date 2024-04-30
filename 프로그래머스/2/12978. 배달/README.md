e1d131d93fcfd553a6e352e6d9d33ffdebcbd86a
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-30 12:41:38 +0900

    [level 2] Title: 배달, Time: 0.41 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 2.36 ms
+메모리: 10.6 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 29일 22:13:38
+2024년 04월 30일 12:41:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,31 +1,23 @@
 from queue import PriorityQueue
-
-def daikstra(N, dic, K):
-    que = PriorityQueue();
-    
+def solution(N, road, K):
+    dic = {i + 1 : [] for i in range(N)}
+    for a,b,c in road:
+        dic[a].append((c,b))
+        dic[b].append((c,a))
+        
     weights = [float('inf')] * (N + 1)
     weights[1] = 0
+    que = PriorityQueue()
     
     que.put((0, 1))
+    
     while que.queue:
-        weight, start = que.get()
-        
-        for n_weight, n in dic.get(start, []):
-            if weight + n_weight < weights[n]:
-                weights[n] = weight + n_weight
-                que.put((weight + n_weight, n))
-
+        p_w, n = que.get()
+        for next_w, next_node in dic[n]:
+            if p_w + next_w >= weights[next_node]:
+                continue
+                
+            weights[next_node] = p_w + next_w
+            que.put((p_w + next_w, next_node))
     
     return len([w for w in weights if w <= K])
-    
-
-def solution(N, road, K):
-    dic = {}
-    for a,b,c in road:
-        dic[a] = dic.get(a, [])
-        dic[a].append((c, b))
-        
-        dic[b] = dic.get(b, [])
-        dic[b].append((c, a))
-        
-    return daikstra(N, dic, K)
