5efc8616df9ea20fbc10299f8d86309fdbfd2111
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-30 14:47:36 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.50 ms
+메모리: 77 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 14:40:51
+2024년 04월 30일 14:47:33
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,48 +1,37 @@
 import java.util.stream.*;
 import java.util.*;
-
 class Solution {
-    public class Node{
-        boolean isVisited = false;
-        List<Node> nodes = new ArrayList<>();
+    boolean[] visited;
+    ArrayList<Integer>[] dic;
+    public void recursive(int point){
+        visited[point] = true;
+        for(int n : dic[point]){
+            if (visited[n]) continue;
+            recursive(n);
+        }
     }
     
     public int solution(int n, int[][] computers) {
-        Node[] nodes = new Node[n + 1];
-        for(int i = 0; i < n + 1; i++){
-            nodes[i] = new Node();
+        visited = new boolean[n];
+        
+        dic = new ArrayList[n];
+        for (int i = 0; i < n; i++){
+            dic[i] = new ArrayList<Integer>();
         }
         
         for(int i = 0; i < computers.length; i++){
-            for(int j = 0; j <computers[0].length; j++){
+            for(int j = 0; j < computers[0].length; j++){
                 if (computers[i][j] == 0) continue;
-                nodes[i + 1].nodes.add(nodes[j + 1]);
-                nodes[j + 1].nodes.add(nodes[i + 1]);
+                dic[i].add(j);
+                dic[j].add(i);
             }
         }
-        
-        LinkedList<Node> que = new LinkedList<>();
-        
         int cnt = 0;
-        nodes[0].isVisited = true;
-        
-        for(Node node : nodes){
-            if (node.isVisited) continue;
-            cnt++;
-            que.addLast(node);
-            
-            while(!que.isEmpty()){
-                Node pollNode = que.pollFirst();
-                for(Node nearNode : pollNode.nodes){
-                    if (nearNode.isVisited) continue;
-                    nearNode.isVisited = true;
-                    que.addLast(nearNode);
-                }
-            }
+        for(int i = 0; i< n; i++){
+            if (visited[i]) continue;
+            recursive(i); cnt++;
         }
         
         return cnt;
-        
-        
     }
 }
