b38fc1ae3521064353f68d558bf974ff263ab098
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-06 16:43:46 +0900

    [level 2] Title: 배달, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 3.62 ms
+메모리: 10.3 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 13:12:30
+2024년 05월 06일 16:43:43
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,23 +1,24 @@
-from queue import PriorityQueue
+from collections import deque
 def solution(N, road, K):
-    dic = {i + 1 : [] for i in range(N)}
-    for a,b,c in road:
-        dic[a].append((c,b))
-        dic[b].append((c,a))
+    weight = [float('inf')] * (N + 1)
+    dic = {i + 1 :[] for i in range(N)}
+    for a, b, c in road:
+        dic[a].append((c, b))
+        dic[b].append((c, a))
         
-    weights = [float('inf')] * (N + 1)
-    weights[1] = 0
-    que = PriorityQueue()
+    que = deque()
+    que.append(1)
+    weight[1] = 0
     
-    que.put((0, 1))
+    while que:
+        node = que.popleft()
+        for w, n in dic[node]:
+            if weight[node] + w < weight[n]:
+                weight[n] = weight[node] + w
+                que.append(n)
+        
+    return len([f for f in weight if f <= K])
     
-    while que.queue:
-        p_w, n = que.get()
-        for next_w, next_node in dic[n]:
-            if p_w + next_w >= weights[next_node]:
-                continue
-                
-            weights[next_node] = p_w + next_w
-            que.put((p_w + next_w, next_node))
+        
     
-    return len([w for w in weights if w <= K])
+        
