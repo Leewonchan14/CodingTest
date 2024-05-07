7c67da45bec0e8bd4c011c238f29ac2c3b676d6c
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-07 15:02:23 +0900

    [level 2] Title: 순위 검색, Time: 3807.24 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -35,8 +35,17 @@ def solution(info, query):
         for key, value in dic.items():
             # q 에 있는 모든 조건이 key 에 포함되어 있으면
             if all([i in key for i in q]):
-                # Use binary search to find the number of scores that are greater or equal to the target score
-                count += len(value) - bisect_left(value, score)
+                len1 = len(value)
+                left = 0
+                right = len1
+                while left < right:
+                    mid = (left + right) // 2
+                    if value[mid] < score:
+                        left = mid + 1
+                    else:
+                        right = mid
+                count += len1 - left
+
 
         result.append(count)
 
