ed9bf0b602c0b3ae554b0caeb11c53815251b463
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-23 22:51:12 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,39 +1,31 @@
 from collections import deque
-def bfs(i, visited, dic):
-    visited[i] = True
-    que = deque()
-    que.append(i)
-    while que:
-        i = que.popleft()
-        for ni in dic.get(i, []):
-            if not visited[ni]:
-                visited[ni] = True
-                que.append(ni)
-        
 
-def dfs(dic, point, visited):
-    visited[point] = True
-    for n in dic[point]:
-        if visited[n]:
-            continue
-        dfs(dic, n, visited)
-        
-        
 def solution(n, computers):
-    dic = [[] for _ in range(n)]
-    for a in range(len(computers)):
-        for b in range(len(computers[0])):
-            if computers[a][b] == 0:
+    dic = {i:[] for i in range(n)}
+    
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j] == 0:
                 continue
-            dic[a].append(b)
-            dic[b].append(a)
-        
-    visited = [False] * n
+            dic[i].append(j)
+            dic[j].append(i)
     
     count = 0
+    visited = [False] * n
     for i in range(n):
-        if not visited[i]:
-            dfs(dic, i, visited)
-            count += 1
-            
+        if visited[i]:
+            continue
+        
+        count += 1
+        visited[i] = True
+        que = deque([i])
+        while que:
+            e = que.popleft() 
+            for ne in dic[e]:
+                if visited[ne]:
+                    continue
+                visited[ne] = True
+                que.append(ne)
+                
     return count
+            
