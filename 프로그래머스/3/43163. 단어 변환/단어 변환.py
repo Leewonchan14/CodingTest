65f7d635cab5e1f90c3963f6f7a490a5b9df1874
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-24 19:16:09 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,23 +1,43 @@
 from collections import deque
 
-def gap3(a,b):
-    return sum([1 if a[i] != b[i] else 0 for i in range(len(a))]) == 1
-
-def bfs(begin, target, words):
-    que = deque()
-    que.append((begin, 0))
-    while que:
-        x, cnt = que.popleft()
-        if x == target:
-            return cnt
+def isGap3(a,b):
+    if len(a) != len(b):
+        return False
+    
+    cnt = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            cnt += 1
+            if cnt >= 2:
+                return False
+    
+    if cnt == 1:
+        return True
+    else:
+        return False
         
-        if cnt >= len(words):
-            return 0
-        
-        for w in words:
-            if gap3(w, x):
-                que.append((w, cnt + 1))
-    return 0
 
 def solution(begin, target, words):
-    return bfs(begin, target, words)
+    words.append(begin)
+    dic = {w: [] for w in words}
+    for i in range(len(words) - 1):
+        for j in range(i, len(words)):
+            if isGap3(words[i], words[j]):
+                dic[words[i]].append(words[j])
+                dic[words[j]].append(words[i])
+                
+    visited = {w : False for w in words}
+    que = deque([(begin, 0)])
+    while que:
+        s,cnt = que.popleft()
+        if s == target:
+            return cnt
+        
+        for e in dic.get(s, []):
+            if visited[e]:
+                continue
+            visited[e] = True
+            que.append((e, cnt + 1))
+            
+    return 0
+        
