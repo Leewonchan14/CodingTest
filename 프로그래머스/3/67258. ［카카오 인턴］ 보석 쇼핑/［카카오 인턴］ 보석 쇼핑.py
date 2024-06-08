d456448db5e5166b0eb27b75d313dfc7ba05f5f5
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-08 11:14:26 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 117.40 ms, Memory: 30.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,35 +1,40 @@
 def solution(gems):
-    dic = {}
-    for i in gems:
-        dic[i] = dic.get(i, 0) + 1
-        
-    target = len(dic.keys())
-    
     left = 0
     right = 0
+    size = len(set(gems))
     
-    deq = {}
-    
-    ls = []
+    dic = {}
+    r = []
     
     while True:
-        while right < len(gems) and len(deq.keys()) != target:
-            deq[gems[right]] = deq.get(gems[right], 0) + 1
+        
+        # 오른쪽 쭉 뻗기
+        while right < len(gems) and len(dic) != size:
+            dic[gems[right]] = dic.get(gems[right] , 0) + 1
             right += 1
-            
-        if len(deq.keys()) != target:
-            break
-            
-        while deq[gems[left]] >= 2:
-            deq[gems[left]] -= 1
-            left += 1
 
-        ls.append([left + 1, right])
-        deq.pop(gems[left])
-        left += 1
+        # 왼쪽 쭉 밀기
+        while left < len(gems) and gems[left] in dic and dic[gems[left]] > 1 and left <= right:
+            dic[gems[left]] -= 1
+            if dic[gems[left]] == 0:
+                dic.pop(gems[left])
+            left += 1
             
-    ls.sort(key=lambda x: (x[1] - x[0], x[0]))
-    
-    return ls[0]
-                
-    
+        # size가 맞으면 추가
+        if size == len(dic):
+            r.append([left, right])
+        
+        # right가 남으면 +1
+        if right < len(gems):
+            dic[gems[right]] = dic.get(gems[right], 0) + 1
+            right += 1
+        # 안남으면 break
+        else:
+            break
+    r.sort(key=lambda x : x[1] - x[0])
+    return [r[0][0] + 1, r[0][1]]
+        
+        
+        
+        
+        
