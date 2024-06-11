df87f61673f17a7034f1ceabf3ad58f7de5e0a11
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-11 11:36:48 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 0.16 ms
+메모리: 10.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 01:53:48
+2024년 06월 11일 11:36:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,30 +1,21 @@
-from queue import PriorityQueue
 from collections import deque
 
 def solution(priorities, location):
-    pr = PriorityQueue()
-    que = deque()
-    for i,p in enumerate(priorities):
-        pr.put((-p, p))
-        que.append((p,i))
-        
     cnt = 0
-        
+    que = deque(enumerate(priorities))
+    li = sorted(priorities)
+    
     while que:
-        task, i = que.popleft()
-        if pr.queue:
-            if pr.queue[0][1] > task:
-                que.append((task, i))
-            else:
-                pr.get()
-                cnt += 1
-                if i == location:
-                    return cnt
+        pop = que.popleft()
+        if li and li[-1] == pop[1]:
+            cnt+=1
+            li.pop()
+            
+            if pop[0] == location:
+                break
+            
+            continue
+            
+        que.append(pop)
         
-        
-    
-        
-    
-    
-    
-    
+    return cnt
