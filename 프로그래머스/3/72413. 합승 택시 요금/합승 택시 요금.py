fecaaba92c8993e12743d72576ac9c75d5cc8d55
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-12 22:21:43 +0900

    [level 3] Title: 합승 택시 요금, Time: 289.74 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,39 +1,20 @@
-from collections import deque
-
-
 def solution(n, s, a, b, fares):
-    dic = {i: {} for i in range(1, n + 1)}
-    for start, end, coast in fares:
-        dic[start][end] = coast
-        dic[end][start] = coast
-
-    result = {}
-
+    arr = [[float('inf')] * (n + 1)  for _ in range(n + 1)]
+    
+    for a1,b1,c1 in fares:
+        arr[a1][b1] = c1
+        arr[b1][a1] = c1
+        
     for i in range(1, n + 1):
-        result[i] = bfs(i, dic)
-
-    result = sorted(result.items(), key=lambda x: x[1][s] + x[1][a] + x[1][b])
-
-    smallest = result[0][1]
-
-    return smallest[a] + smallest[b] + smallest[s]
-
-
-def bfs(start, dic):
-    result = {i + 1: float('inf') for i in range(len(dic))}
-
-    result[start] = 0
-
-    que = deque()
-    que.append((start, 0))
-    while que:
-        idx, now_coast = que.popleft()
-
-        for next_node, coast in dic[idx].items():
-            if now_coast + coast < result[next_node]:
-                result[next_node] = now_coast + coast
-                que.append((next_node, result[next_node]))
-
-    return result
-
-
+        arr[i][i] = 0
+        
+    for k in range(1, n + 1):
+        for a1 in range(1, n + 1):
+            for b1 in range(1, n + 1):
+                arr[a1][b1] = min(arr[a1][b1], arr[a1][k] + arr[k][b1])
+    
+    minv = float('inf')
+    for k in range(1, n + 1):
+        minv = min(minv, arr[s][k] + arr[a][k] + arr[b][k])
+        
+    return minv
