4de2f008b609e52e6705391adaff5fde971c4552
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-12 18:09:10 +0900

    [Silver III] Title: 1로 만들기, Time: 772 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -4,13 +4,12 @@ sys.setrecursionlimit(10 ** 6)
 
 N = int(input())
 
-dp = {1: 0}
+dp = [0, 0]
 
 for i in range(2, N + 1):
     a = dp[i - 1] + 1
     b = dp[i // 2] + 1 if i % 2 == 0 else float('inf')
     c = dp[i // 3] + 1 if i % 3 == 0 else float('inf')
-
-    dp[i] = min(a, b, c)
+    dp.append(min(a, b, c))
 
 print(dp[N])
