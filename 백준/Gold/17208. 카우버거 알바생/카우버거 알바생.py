9cf230109263b0c2ea16d3e2a49cc9a63e3193e6
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-26 11:39:46 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 8076 ms, Memory: 104444 KB -BaekjoonHub

--- 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.py
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.py
@@ -6,31 +6,21 @@ n, m, k, = map(int, input().split())
 
 li = [tuple(map(int, input().split())) for _ in range(n)]
 
-dp = [[[-1] * (k + 1) for _ in range(m + 1)] for _ in range(n + 1)]
+dp = [[[0] * (k + 1) for _ in range(m + 1)] for _ in range(n + 1)]
 
+r = 0
 
-def knapsack(index, c, g):
-    if index >= n:
-        return 0
+for i in range(1, n + 1):
+    for c in range(1, m + 1):
+        for g in range(1, k + 1):
+            n_c, n_g = li[i - 1]
 
-    if dp[index][c][g] != -1:
-        return dp[index][c][g]
+            if c >= n_c and g >= n_g:
+                dp[i][c][g] = max(dp[i - 1][c][g], dp[i - 1][c - n_c][g - n_g] + 1)
 
-    n_c, n_g = li[index]
+            else:
+                dp[i][c][g] = dp[i - 1][c][g]
 
-    choose, no_choose = 0, 0
+            r = max(r, dp[i][c][g])
 
-    # index 주문이 가능 하다면 index 주문을 주문 했을 때 경우의 수
-    if c - n_c >= 0 and g - n_g >= 0:
-        choose = knapsack(index + 1, c - n_c, g - n_g) + 1
-
-    # index 주문 안 받았을 때 경우의 수
-    no_choose = knapsack(index + 1, c, g)
-
-    # index개 까지 주문을 봤을때 남은 치즈 c, 남은 감튀 g개 일때 받은 주문의 최댓값
-    dp[index][c][g] = max(choose, no_choose)
-
-    return dp[index][c][g]
-
-
-print(knapsack(0, m, k))
+print(r)
