21f255642eda0fcf57c7f99c18aeb72f63e94e12
이원찬 <twoone14@tukorea.ac.kr>
2024-09-18 12:39:24 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,4 +1,5 @@
 function solution(array) {
-    array.sort((a,b) => a -b )
-    return array[Math.floor(array.length / 2)]                
+    array.sort((a,b) => a - b)
+    
+    return array[Math.floor(array.length / 2)]
 }
