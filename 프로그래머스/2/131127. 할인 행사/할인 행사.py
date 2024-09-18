599069a87a17dc6c5fd2328b51fde99cd0d50c91
이원찬 <twoone14@tukorea.ac.kr>
2024-09-18 17:45:15 +0900

    [level 2] Title: 할인 행사, Time: 66.35 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,32 +1,37 @@
 def solution(want, number, discount):
-    count = sum(number)
-    target = {want[i]: number[i] for i in range(len(want))}
-    window = {}
-    for i in range(count):
-        add_item = discount[i]
-        window[add_item] = window.get(add_item, 0) + 1
-
-    delete_index = 0
-    add_index = delete_index + count
-    rt = 0
-    while True:
-        if all([w_k in target and target[w_k] == w_v for w_k, w_v in window.items()]):
-            rt += 1
-
-        if add_index >= len(discount):
-            break
-
-        delete_item = discount[delete_index]
-        if window[delete_item] > 1:
-            window[delete_item] -= 1
-        else:
-            window.pop(delete_item)
-
-        add_item = discount[add_index]
-        window[add_item] = window.get(add_item, 0) + 1
-
-        delete_index += 1
-        add_index += 1
-
-    return rt
+    want = dict(zip(want, number))
+    cnt = 0
+    
+    for i in range(10):
+        if discount[i] in want:
+            want[discount[i]] -= 1
+            
+    if all([v <= 0 for v in want.values()]):
+        cnt += 1
+    
+    for i in range(len(discount) - 10):
+        if discount[i] in want:
+            want[discount[i]] = want.get(discount[i], 0) + 1
+        
+        if discount[i + 10] in want:
+            want[discount[i + 10]] -= 1
+        
+        if all([v <= 0 for v in want.values()]):
+            cnt += 1
+            
+    return cnt
+        
+        
+            
+        
+        
+        
+            
+    
+        
+        
+        
+    
+    
+    
 
