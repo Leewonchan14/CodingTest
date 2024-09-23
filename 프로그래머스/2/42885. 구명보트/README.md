a7a3bccc731936ac1b6bc41e4aee186aa9198270
이원찬 <twoone14@tukorea.ac.kr>
2024-09-23 13:42:07 +0900

    [level 2] Title: 구명보트, Time: 11.44 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 33.01 ms
+메모리: 54 MB, 시간: 11.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 15:48:06
+2024년 09월 23일 13:42:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,41 +1,20 @@
 import java.util.*;
 
 class Solution {
-    int[] counts = new int[300];
     public int solution(int[] people, int limit) {
-        int max = 0;
-        int min = 300;
-        for(int weight : people){
-            counts[weight]++;
-            if (weight > max) max = weight;
-            if (weight < min) min = weight;
+        Arrays.sort(people);
+        int right = people.length - 1;
+        int left = 0;
+        int result = 0;
+        
+        while (left <= right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+            result++;
+            right--;
         }
         
-        int[] boat = new int[2];
-        int cnt = 0;
-        while(true){
-            cnt += 1;
-                
-            if (max + min <= limit && !(min == max && counts[min] <= 1)){
-                counts[max] -= 1;
-                counts[min] -= 1;
-            } else {
-                counts[max] -= 1;
-            }
-            
-            while(0 <= max && counts[max] == 0){
-                max -= 1;
-            }
-            while(min < 300 && counts[min] == 0){
-                min += 1;
-            }
-            if (max == -1 || min == 300) break;
-            
-            if(min == max && counts[max] == 0){
-                break;
-            }
-        }
-        
-        return cnt;
+        return result;
     }
 }
