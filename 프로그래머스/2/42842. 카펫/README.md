dff0d05094530ddd39c98880198778535fe3b3c1
이원찬 <twoone14@tukorea.ac.kr>
2024-09-23 13:26:30 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.36 ms
+메모리: 71.4 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 15:03:17
+2024년 09월 23일 13:26:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,20 +2,14 @@ import java.util.stream.*;
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        boolean[] isSo = new boolean[yellow + 1];
-        Arrays.fill(isSo,true);
-        
-        for(int i = 1; i * i <= yellow; i++){
-            // 
-            if(yellow % i != 0){
-                continue;
-            } 
+        for(int h = 1; h <= Math.sqrt(yellow); h++){
+            float w1 = yellow / (float)h;
+            int w2 = yellow / h;
             
-            int row = i;
-            int col = yellow / i;
-            
-            if(2*row + 2*col + 4 == brown){
-                return new int[]{(row<col ? col:row) + 2, (row<col ? row:col) + 2};
+            if (w1 != w2) continue;
+                
+            if (w2 * 2 + h * 2 + 4 == brown){
+                return new int[]{w2 + 2, h + 2};
             }
         }
         
