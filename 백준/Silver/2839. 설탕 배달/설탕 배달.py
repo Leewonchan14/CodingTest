e2c1abd5ce634afb0c448f18f65ba68f2782f0e8
이원찬 <twoone14@tukorea.ac.kr>
2024-09-27 18:41:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,11 +1,12 @@
 n = int(input())
-f = n // 5
-div = n % 5
-an = -1
-for i in range(f, -1 , -1):
-    if div % 3 == 0:
-        an = i + div // 3
-        break
-    div += 5
 
-print(an)
+dp = [-1, -1, -1, 1, -1, 1]
+
+for i in range(6, n + 1):
+    if dp[i - 5] > 0 and dp[i - 3] > 0:
+        dp.append(min(dp[i - 5], dp[i - 3]) + 1)
+    elif dp[i - 5] > 0 or dp[i - 3] > 0:
+        dp.append(max(dp[i - 5], dp[i - 3]) + 1)
+    else:
+        dp.append(-1)
+print(dp[n])
