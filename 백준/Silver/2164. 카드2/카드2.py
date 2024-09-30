ea1560ad91eb69ca0db82ce24e064a0d15e44e1e
이원찬 <twoone14@tukorea.ac.kr>
2024-09-30 17:10:28 +0900

    [Silver IV] Title: 카드2, Time: 208 ms, Memory: 54996 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,22 +1,13 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-# 카드는 제일 위에서부터 1234 의 순서로 놓여있다.
-# 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다.
-# 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다.
-# 마지막으로 2를 버리고 나면,
-# 남는 카드는 4가 된다.
 n = int(input())
-li = list(range(n, 0, - 1))
-que = deque(li)
-while True:
-    if len(que) == 1:
-        break
 
-    que.pop()
+que = deque([i for i in range(1, n + 1)])
 
-    if len(que) == 1:
-        break
-
-    que.insert(0, que.pop())
-
-print(que[0])
+while len(que) != 1:
+    que.popleft()
+    que.append(que.popleft())
+    
+print(que.popleft())
