a2d8229f6f52a376ce21c2894eaf27376e094828
이원찬 <twoone14@tukorea.ac.kr>
2024-09-30 17:01:04 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,11 +1,14 @@
 import sys
 
-n = int(input())
-li = []
-for i in range(n):
-    s = int(sys.stdin.readline().rstrip())
-    if s == 0:
-        li.pop()
-    else:
-        li.append(s)
-print(sum(li))
+input = sys.stdin.readline
+
+stk = []
+
+for _ in range(int(input())):
+    n = int(input())
+    if n == 0:
+        stk.pop()
+        continue
+    stk.append(n)
+    
+print(sum(stk))
