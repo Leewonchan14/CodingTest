85d2b825b1ce84a9b92d71e9e081162d28f6480d
이원찬 <twoone14@tukorea.ac.kr>
2024-10-04 12:23:15 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,29 +1,28 @@
 from collections import deque
 
-def is_can_go(star, end):
-    return sum([int(a != b) for a, b in zip(star, end)]) == 1
-            
-    return cnt == 1
+def is_one(start, end):
+    if len(start) != len(end):
+        return False
+    
+    return sum([1 if start[i] != end[i] else 0 for i in range(len(start))]) == 1
+
+def bfs(visited, start, word, end):
+    que = deque([(start, 0)])
+    while que:
+        item, k = que.popleft()
+        
+        if item == end:
+            return k
+        
+        for w in word:
+            if visited[w] or not is_one(item, w):
+                continue
+            visited[w] = True
+            que.append((w, k + 1))
+    
+    return 0
+    
 
 def solution(begin, target, words):
-    que = deque()
-    
     visited = {w: False for w in words}
-    
-    visited[begin] = True
-    
-    que.append((begin, 0))
-    
-    while que:
-        start, cnt = que.popleft()
-        
-        if start == target:
-            return cnt
-        
-        for w in words:
-            if not visited[w] and is_can_go(start, w):
-                visited[w] = True
-                que.append((w, cnt + 1))
-                
-    return 0
-        
+    return bfs(visited, begin, words, target)
