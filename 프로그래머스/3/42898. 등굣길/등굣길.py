2b40a92306f513bab3408587b9e4882e1e103320
이원찬 <twoone14@tukorea.ac.kr>
2024-10-04 12:41:54 +0900

    [level 3] Title: 등굣길, Time: 2.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,29 +1,20 @@
 def solution(m, n, puddles):
-    dp = [[0] * m for _ in range(n)]
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
     
-    dp[0][0] = 1
+    dp[1][1] = 1
     
     for w,h in puddles:
-        dp[h - 1][w - 1] = -1
+        dp[h][w] = -1
         
-    for h in range(n):
-        for w in range(m):
-            if h == 0 and w == 0:
+    for h in range(1, n + 1):
+        for w in range(1, m + 1):
+            if h == 1 and w == 1:
                 continue
             
             if dp[h][w] == -1:
                 dp[h][w] = 0
                 continue
                 
-            if h == 0:
-                dp[h][w] = dp[h][w - 1]
-                continue
-                
-            if w == 0:
-                dp[h][w] = dp[h - 1][w]
-                continue
-            
-            
             dp[h][w] = (dp[h - 1][w] + dp[h][w - 1]) %  1000000007
             
             
