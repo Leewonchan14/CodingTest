172b85596175a03239ea4797af49265d3a1795c1
이원찬 <twoone14@tukorea.ac.kr>
2024-10-04 14:31:01 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 564.83 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 3] 연속 펄스 부분 수열의 합 - 161988 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 402.71 ms
+메모리: 85.5 MB, 시간: 564.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 01일 12:47:58
+2024년 10월 04일 14:30:59
 
 ### 문제 설명
 
--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,12 +1,17 @@
-def solve(sequence):
-    for i in range(1, len(sequence)):
-        sequence[i] = max(sequence[i - 1] + sequence[i], sequence[i])
-    
-    return max(sequence)
-    
-
 def solution(sequence):
-    length = len(sequence)
-    a = [(-1 if i % 2 == 0 else 1) * sequence[i] for i in range(length)]
-    b = [(-1 if i % 2 != 0 else 1) * sequence[i] for i in range(length)]
-    return max(solve(a), solve(b))
+    purse1 = [1 if i % 2 == 0 else -1 for i in range(len(sequence))]
+    purse2 = [-1 if i % 2 == 0 else 1 for i in range(len(sequence))]
+    
+    dp1 = list(map(lambda x: x[0] * x[1], zip(sequence, purse1)))
+    dp2 = list(map(lambda x: x[0] * x[1], zip(sequence, purse2)))
+    
+    for i in range(1, len(sequence)):
+        dp1[i] = max(dp1[i - 1]+dp1[i], dp1[i])
+        
+    for i in range(1, len(sequence)):
+        dp2[i] = max(dp2[i - 1]+dp2[i], dp2[i])
+        
+    
+        
+    return max(max(dp1), max(dp2))
+    
