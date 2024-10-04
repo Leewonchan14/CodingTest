dbef912ab20b83ddcde9a967a6fbd0062c4ba772
이원찬 <twoone14@tukorea.ac.kr>
2024-10-04 14:02:09 +0900

    [level 3] Title: 가장 먼 노드, Time: 39.93 ms, Memory: 22 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,31 +1,28 @@
 from collections import deque
-def bfs(start, visited, dic):
-    que = deque()
-    visited[start] = True
-    que.append((start, 0))
-    
-    c_dic = {}
-    
-    while que:
-        start, cnt = que.popleft()
-        c_dic[cnt] = c_dic.get(cnt, 0) + 1
-        for n in dic[start]:
-            if not visited[n]:
-                visited[n] = True
-                que.append((n, cnt + 1))
-                
-    return sorted(c_dic.items())[-1][1]
-    
+
 def solution(n, edge):
-    dic = {}
-    visited = {}
-    for a,b in edge:
-        visited[a] = False
-        dic[a] = dic.get(a, [])
+    w = [0] * (n + 1)
+    
+    dic = {i: [] for i in range(1, n + 1)}
+    visited = [False] * (n + 1)
+    visited[1] = True
+    
+    for a, b in edge:
         dic[a].append(b)
-        
-        visited[b] = False
-        dic[b] = dic.get(b, [])
         dic[b].append(a)
-        
-    return bfs(1, visited, dic)
+    
+    que = deque([(1, 0)])
+    
+    max_v = 0
+    while que:
+        start, k = que.popleft()
+        for end in dic[start]:
+            if visited[end]:
+                continue
+                
+            visited[end] = True
+            w[end] = k + 1
+            que.append((end, k + 1))
+            max_v = max(max_v, k + 1)
+    
+    return len([i for i in w if i == max_v])
