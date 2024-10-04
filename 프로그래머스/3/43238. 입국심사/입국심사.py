7d3a37f72b5b0a5981886a3839438c0435fdc2be
이원찬 <twoone14@tukorea.ac.kr>
2024-10-04 14:23:11 +0900

    [level 3] Title: 입국심사, Time: 778.94 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,23 +1,15 @@
-def cal(mid, times):
-    return sum([mid // t for t in times])
-
 def solution(n, times):
+    right = max(times) * n
     left = 0
-    right = 10000000000000000000
-    # right = 50
-    mid = (left + right) // 2
     
-    while left != right:
-    # for i in range(15):
-        r = cal(mid, times)
-        if r < n:
-            left = mid + 1
-        else:
-            right = mid
-            
+    while left < right:
         mid = (left + right) // 2
+        cal = sum([mid // t for t in times])
         
-        # print(left, mid, right)
+        if cal >= n:
+            right = mid
+        else:
+            left = mid + 1
+            
+    return left
         
-    return mid
-    
