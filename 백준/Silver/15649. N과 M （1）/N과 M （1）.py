adae2e42331a6a24f7f7b4e818715de95ab26c2f
이원찬 <twoone14@tukorea.ac.kr>
2024-10-05 13:05:11 +0900

    [Silver III] Title: N과 M (1), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,6 +1,4 @@
 import itertools
-
-n, a = map(int, input().split())
-
-for i in itertools.permutations(map(str, range(1, n + 1)), a):
+n,m = map(int, input().split())
+for i in itertools.permutations(map(str, range(1, n + 1)), m):
     print(" ".join(i))
