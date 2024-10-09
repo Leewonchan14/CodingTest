329ff08831351295c97f8193cbdb7d2ad902a873
이원찬 <twoone14@tukorea.ac.kr>
2024-10-09 17:28:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 92.59 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 131.41 ms
+메모리: 16.5 MB, 시간: 92.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 20일 19:15:40
+2024년 10월 09일 17:28:23
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,65 +1,36 @@
-"""
-1. 아이디어
-    - 정렬이 되어있는 deque을 이용해 insert, delete 연산을 하며 마지막에 첫원소, 마지막 원소를 반환한다.
-    - 원소를 집어넣을때는 이분탐색을 이용한다.
-
-2. 시간 복잡도
-    - insert : log(n)
-    - 최댓값, 최소값 delete : 1
-    1000000 의 경우의 수를 log(n)번
-    10 ** 6 == 2 ** 20
-    최악의수 => n * log(n)
-    => 1000000 * 20 = 2천만 == 가능
-
-3. 자료 구조
-    que : Deque
-"""
-
+import sys
 from collections import deque
 
 
-def index(que, start, end, value):
-    if len(que) == 0:
-        return 0
-    
-    mid = (start + end) // 2
-    
-    if start == end:
-        return start
-    
-    if value <= que[mid]:
-        return index(que, start, mid, value)
-        
-    elif value > que[mid]:
-        return index(que, mid + 1, end, value)
-    
-    
-    
+def find_index(li, value):
+    left = 0
+    right = len(li)
+
+    while left < right:
+        mid = (left + right) // 2
+        if value < li[mid]:
+            right = mid
+        elif value > li[mid]:
+            left = mid + 1
+        else:
+            return mid
+
+    return left
 
 
 def solution(operations):
-    que = deque()
-    
-    # print(dir(que))
-    
-    for item in operations:
-        oper, value = item.split()
-        value = int(value)
-        
-        if oper == "I":
-            idx = index(que, 0, len(que), value)
-            que.insert(idx, value)
-            
-        elif oper == "D":
-            if que:
-                if value == -1:
-                    que.popleft()
-                elif value == 1:
-                    que.pop()
-        
-    if que:
-        return [que[-1], que[0]]
-    else:
-        return [0, 0]
-                
+    li = deque()
+    for i in operations:
+        op, v = i.split()
+        v = int(v)
+        if op == "I":
+            li.insert(find_index(li, v), v)
+
+        elif op == "D":
+            if v == -1 and li:
+                li.popleft()
+            elif v == 1 and li:
+                li.pop()
+
+    return [li[-1], li[0]] if li else [0, 0]
 
