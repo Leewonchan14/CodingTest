5941c20fa2e986173aeba44fd483358d9ca752a8
이원찬 <twoone14@tukorea.ac.kr>
2024-12-06 18:40:23 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,32 +1,10 @@
-import itertools
-
-def mul(ls):
-    rt = 1
-    for i in ls:
-        rt *= i
-    return rt
-
-sumv = [0]
-ls = []
-
-def track(num, N, visited, dic):
-    if num == N:
-        sumv[0] += mul(ls)
-        print(ls)
-        return 
-    
-    for k in dic.keys():
-        if not visited[k]:
-            visited[k] = True
-            ls.append(dic[k])
-            track(num + 1, N, visited, dic)
-            visited[k] = False
-            ls.pop()
-        
-
 def solution(clothes):
     dic = {}
-    for name, type in clothes:
-        dic[type] = dic.get(type, 0) + 1
-        
-    return mul([i + 1 for i in dic.values()]) - 1
+    for v, k in clothes:
+        dic[k] = dic.get(k, 0) + 1
+    
+    mul =  1
+    for i in dic.values():
+        mul *= i + 1
+    
+    return mul - 1
