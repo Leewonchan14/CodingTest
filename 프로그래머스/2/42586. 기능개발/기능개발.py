c8c3127af780d0e18edc54f685cc28c2f985add9
이원찬 <twoone14@tukorea.ac.kr>
2024-12-06 18:47:14 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,21 @@
 import math
+
 def solution(progresses, speeds):
-    index = 0
-    ls = []
-    days = 0
-    while True:
-        days += math.ceil((100 - progresses[index]) / speeds[index])
-        count = 0
-        flag = False
-        while progresses[index] + speeds[index] * days >= 100:
-            index += 1
-            count += 1
-            if index >= len(progresses):
-                flag = True
-                break
-            continue
-        ls.append(count)
-        if flag:
-            break
-        progresses[index] += speeds[index] * days
-    
-    return ls
-            
+    answer = []
+    i = 0
+    while i < len(progresses):
+        s = speeds[i]
+        p = progresses[i]
         
+        
+        cnt = 0
+        t = math.ceil((100 - p) / s)
+        while i < len(progresses) and progresses[i] + speeds[i] * t >= 100:
+            cnt += 1
+            i += 1
+            
+        answer.append(cnt)
+                
+    return answer
         
         
