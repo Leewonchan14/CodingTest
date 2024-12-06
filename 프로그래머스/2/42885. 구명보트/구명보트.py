208e224014bca648d4ef9cbe627f1508c9b6f3c6
이원찬 <twoone14@tukorea.ac.kr>
2024-12-06 16:14:41 +0900

    [level 2] Title: 구명보트, Time: 12.47 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,23 +1,19 @@
+from collections import deque
+
 def solution(people, limit):
-    people.sort()
-    small_idx = 0
-    big_idx = len(people) - 1
-    
-    count = 0
-    
-    while True:
-        if small_idx > big_idx:
-            break
-            
-        small = people[small_idx]
-        big = people[big_idx]
-        
-        if small + big <= limit:
-            small_idx += 1
-        
-        big_idx -= 1
-        count += 1
-        
-    return count
-        
-        
+    people.sort()  # 사람들의 몸무게를 정렬
+    people = deque(people)  # 양쪽에서 요소를 제거하기 위해 deque 사용
+    boats = 0  # 필요한 보트 수
+
+    while people:
+        # 양 끝을 조합하여 제한을 초과하지 않는다면 둘 다 태움
+        if len(people) >= 2 and people[0] + people[-1] <= limit:
+            people.popleft()
+            people.pop()
+        else:
+            # 그렇지 않으면 무거운 사람(끝쪽)을 태움
+            people.pop()
+
+        boats += 1  # 보트 사용 증가
+
+    return boats
