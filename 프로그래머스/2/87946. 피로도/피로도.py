fac6ee9ad158eef785b3bb1bf3734b4039a92977
이원찬 <twoone14@tukorea.ac.kr>
2024-12-06 20:55:38 +0900

    [level 2] Title: 피로도, Time: 50.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,23 @@
-def back(k, dungeons, visited, length, ls):
-    ls[-1] = max(ls[-1], length)
-    for i in range(len(dungeons)):
-        if not visited[i] and k >= dungeons[i][0]:
-            visited[i] = True
-            back(k - dungeons[i][1], dungeons, visited, length + 1, ls)
-            visited[i] = False
+_max = [0]
+
+def recursive(k, dungeons, visited, c):
+    _max[0] = max(_max[0], c)
+    
+    for i, d in enumerate(dungeons):
+        if visited[i]:
+            continue
+            
+        if k < d[0] or k < d[1]:
+            continue
+        
+        visited[i] = True
+        recursive(k - d[1], dungeons, visited, c + 1)
+        visited[i] = False
+        
 
 def solution(k, dungeons):
+    dungeons.sort(key=lambda x: x[1])
     visited = [False] * len(dungeons)
-    ls = [0]
-    back(k, dungeons, visited, 0, ls)
-    return ls[-1]
+    recursive(k, dungeons, visited, 0)
+    
+    return _max[0]
