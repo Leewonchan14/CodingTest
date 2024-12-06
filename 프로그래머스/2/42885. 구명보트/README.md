e85cedb59ab32b83b180607bf214310ed976c521
이원찬 <twoone14@tukorea.ac.kr>
2024-12-06 16:20:52 +0900

    [level 2] Title: 구명보트, Time: 12.67 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 10.7 MB, 시간: 12.47 ms
+메모리: 10.6 MB, 시간: 12.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 06일 16:14:39
+2024년 12월 06일 16:20:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,23 @@
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+
 from collections import deque
+    
+    
 
 def solution(people, limit):
-    people.sort()  # 사람들의 몸무게를 정렬
-    people = deque(people)  # 양쪽에서 요소를 제거하기 위해 deque 사용
-    boats = 0  # 필요한 보트 수
-
+    people.sort()
+    people = deque(people)
+    k = 0
     while people:
-        # 양 끝을 조합하여 제한을 초과하지 않는다면 둘 다 태움
         if len(people) >= 2 and people[0] + people[-1] <= limit:
             people.popleft()
             people.pop()
+
         else:
-            # 그렇지 않으면 무거운 사람(끝쪽)을 태움
             people.pop()
-
-        boats += 1  # 보트 사용 증가
-
-    return boats
+        
+        k += 1
+        
+    return k
