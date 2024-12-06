01df604b7403e9611d06e2f4a4a7aa3d398baf6f
이원찬 <twoone14@tukorea.ac.kr>
2024-12-06 14:49:13 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,8 @@
-def solution(n):
-    bin_num = bin(n)
-    zero_count = bin(n).count("1")
-    next_n = n + 1
-    while bin(next_n).count("1") != zero_count:
-        next_n += 1
-    return next_n 
-        
+def recursive(n, oneCnt):
+    if bin(n)[2:].count("1") == oneCnt:
+        return n
     
+    return recursive(n + 1, oneCnt)
+
+def solution(n):
+    return recursive(n + 1, bin(n).count("1"))
