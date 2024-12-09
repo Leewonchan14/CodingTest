88599b5e46d57e7f6395b80faa790f8da4860910
이원찬 <twoone14@tukorea.ac.kr>
2024-12-09 15:50:38 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 332.51 ms, Memory: 318 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,19 +1,32 @@
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+
 from collections import deque
 
-def solution(bridge_length, weight, truck_weights):
-    none = deque(truck_weights)
-    brige = deque([0] * bridge_length)
-    time = 0
-    sumv = 0
-    while none or sumv != 0:
-        time += 1
-        if brige:
-            sumv -= brige.popleft()
-            
-        if none and sumv + none[0] <= weight:
-            sumv += none[0]
-            brige.append(none.popleft())
-        else:
-            brige.append(0)
+def recursive(bridge_length, weight, que, time, bridge, now_bridge_length, now_weight):
+    if len(que) == 0 and now_weight == 0:
+        return time
+    
+    if bridge:
+        pop = bridge.popleft()
+        now_bridge_length -= 1
+        now_weight -= pop
+    
+    # 트럭이 올라탈수 있다면
+    if now_bridge_length + 1 <= bridge_length and que and now_weight + que[0] <= weight:
+        pop = que.popleft()
+        bridge.append(pop)
+        now_bridge_length += 1
+        now_weight += pop
         
-    return time
+    else:
+        bridge.append(0)
+        
+    return recursive(bridge_length, weight, que, time + 1, bridge, now_bridge_length, now_weight)
+    
+    
+
+def solution(bridge_length, weight, truck_weights):
+    que = deque(truck_weights)
+    return recursive(bridge_length, weight, que, 0, deque([0] * bridge_length), 0, 0)
