5d63bb1983a62b01627152356838323e019c06b9
이원찬 <twoone14@tukorea.ac.kr>
2024-12-10 18:14:50 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,20 +1,21 @@
+li = [1, 1, 2, 4]
+
+
+def recursive(n):
+    if n <= 3:
+        return str(li[n])
+
+    post = ""
+    if n % 3 == 0:
+        n = n // 3 - 1
+        post = "4"
+    else:
+        post = str(li[n % 3])
+        n //= 3
+
+    return recursive(n) + post
+
+
 def solution(n):
-    ls = []
-    mapper = ["0","1", "2", "4"]
-
-    while True:
-        div = n % 3
-        if div == 0:
-            n = n // 3 - 1
-            ls.append("4")
-        else:
-            ls.append(mapper[n % 3])
-            n = n // 3
-
-        if n == 0:
-            break
-
-    ls.reverse()
-
-    return "".join(ls)
+    return recursive(n)
 
