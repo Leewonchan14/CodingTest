4ae79b1c193d2ab373bd822569bafbdee9914ea4
이원찬 <twoone14@tukorea.ac.kr>
2024-12-10 22:16:50 +0900

    [level 2] Title: 무인도 여행, Time: 22.40 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,43 +1,33 @@
-from collections import deque
+import sys
 
+sys.setrecursionlimit(10 ** 9)
 
-def bfs(y, x, visited, maps):
-    dy = [0, 1, 0, -1]
-    dx = [1, 0, -1, 0]
+def isCorrectPoint(y, x, maps, visited):
+    return 0 <= y < len(maps) and 0 <= x < len(maps[y]) and maps[y][x] != "X" and not visited[y][x] 
 
-    que = deque()
-    que.append((y, x))
-    visited[y][x] = True
-    cnt = 0
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
 
-    while que:
-        y, x = que.popleft()
-        cnt += int(maps[y][x])
-
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-
-            if 0 <= ny < len(maps) and 0 <= nx < len(maps[ny]):
-                if not visited[ny][nx] and maps[ny][nx] != "X":
-                    visited[ny][nx] = True
-                    que.append((ny, nx))
-
-    return cnt
-
+def dfs(y, x, maps, visited):
+    sum = int(maps[y][x])
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if isCorrectPoint(ny, nx, maps, visited):
+            visited[ny][nx] = True
+            sum += dfs(ny, nx, maps, visited)
+    
+    return sum
 
 def solution(maps):
-    visited = [[False] * len(i) for i in maps]
-    maps = list(map(lambda x: list(x), maps))
-
-    rs = []
-
-    for y in range(len(maps)):
-        for x in range(len(maps[y])):
-            if not visited[y][x] and maps[y][x] != "X":
-                rs.append(bfs(y, x, visited, maps))
-
-    rs.sort()
-
-    return rs if rs else [-1]
-
+    result = []
+    visited = [[False] * len(r) for r in maps]
+    for r in range(len(maps)):
+        for c in range(len(maps[r])):
+            if isCorrectPoint(r, c, maps, visited):
+                visited[r][c] = True
+                result.append(dfs(r, c, maps, visited))
+                
+    result.sort()
+    
+    return result if result else [-1]
