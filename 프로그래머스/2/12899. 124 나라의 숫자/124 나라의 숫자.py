ce8b8f9b58b0d0cefbe30ad4346e12f430bd2d31
이원찬 <twoone14@tukorea.ac.kr>
2024-12-10 19:40:00 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,21 +1,12 @@
-li = [1, 1, 2, 4]
-
-
 def recursive(n):
     if n <= 3:
-        return str(li[n])
-
-    post = ""
+        return "4124"[n]
+    
+    nn = n // 3
     if n % 3 == 0:
-        n = n // 3 - 1
-        post = "4"
-    else:
-        post = str(li[n % 3])
-        n //= 3
-
-    return recursive(n) + post
-
+        nn -= 1
+        
+    return recursive(nn) + "412"[n % 3]
 
 def solution(n):
     return recursive(n)
-
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899#qna) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 10일 18:14:49
+2024년 12월 10일 19:39:57
 
 ### 문제 설명
 
