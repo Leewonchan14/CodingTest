301a5d31fa222c0b0cd1b97b4b5997ae37f0353f
이원찬 <twoone14@tukorea.ac.kr>
2024-12-10 23:12:07 +0900

    [level 2] Title: 괄호 변환, Time: 1.62 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,67 +1,32 @@
-from collections import deque
+def reverse(m):
+    dic = {"(": ")", ")" : "("}
+    return "".join([dic[i] for i in m])
 
-
-def is_collect(s):
+def is_collect(m):
     stk = []
-    index = 0
-    while index < len(s):
-
-        if not stk or s[index] == "(":
-            stk.append(s[index])
-            index += 1
-            continue
-
-        if s[index] == ")":
-            index += 1
+    for i in m:
+        if i == ")":
+            if not stk or stk[-1] != "(":
+                return False
             stk.pop()
-
+        else:
+            stk.append(i)
+            
     return len(stk) == 0
 
-
-def div_blance(s):
-    count_9 = 0
-    count_0 = 0
-    index = 0
-    flag = False
-    while index < len(s):
-        if s[index] == "(":
-            count_9 += 1
-        elif s[index] == ")":
-            count_0 += 1
-
-        index += 1
-        if count_9 == count_0:
-            flag = True
-            break
-
-    if flag and len(s) != index:
-        return s[:index], s[index:]
-
-    if flag and len(s) == index:
-        return s, ""
-
-
-def dfs(s):
-    mapper = {"(": ")", ")": "("}
-
-    if s == "":
-        return s
-
-    if is_collect(s):
-        return s
-
-    u, v = div_blance(s)
-
-    if is_collect(u):
-        return u + dfs(v)
-
-    a = "(" + dfs(v) + ")"
-    b = "".join(list(map(lambda x: mapper[x], list(u[1:-1]))))
-
-    return a + b
-
+def to_collect(m):
+    if is_collect(m):
+        return m
+    
+    for i in range(1, len(m) + 1):
+        u = m[0:i]
+        if u.count("(") == u.count(")"):
+            v = m[i:]
+            if is_collect(u):
+                return u + to_collect(v)
+            
+            return f"({to_collect(v)})" + reverse(u[1:-1])
+            
 
 def solution(p):
-    return dfs(p)
-
-# print(solution(")("))
+    return to_collect(p)
