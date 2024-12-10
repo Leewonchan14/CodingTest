7ee794a8562b267607adf145da8edee336c62d04
이원찬 <twoone14@tukorea.ac.kr>
2024-12-10 16:43:20 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 15:43:46
+2024년 12월 10일 16:43:18
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,27 +1,23 @@
+def recursive(num):
+    if num == 0:
+        return 0
+    
+    n10 = num % 100 // 10
+    n1 = num % 10
+    
+    if n1 < 5:
+        return n1 + recursive(num // 10)
+    
+    if n1 > 5:
+        return 10 - n1 + recursive(num // 10 + 1)
+    
+    if n1 == 5 and n10 >= 5:
+        return 10 - n1 + recursive(num // 10 + 1)
+    
+    if n1 == 5 and n10 < 5:
+        return n1 + recursive(num // 10)
+    
+    
+
 def solution(storey):
-    cnt = 0
-    while storey != 0:
-        n = storey % 10
-        if n < 5:
-            cnt += n
-            storey //= 10
-            continue
-
-        if n > 5:
-            cnt += 10 - n
-            storey = storey // 10 + 1
-            continue
-
-        if n == 5:
-            # 앞 숫자
-            front = storey // 10 % 10
-            if front < 5:
-                cnt += 5
-                storey //= 10
-                continue
-            elif front >= 5:
-                storey = storey // 10 + 1
-                cnt += 5
-                continue
-                
-    return cnt  
+    return recursive(storey)
