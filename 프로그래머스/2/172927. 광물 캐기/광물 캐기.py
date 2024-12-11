0fd275792786f1b62c2c0e5e0a5848dc8459f7b0
이원찬 <twoone14@tukorea.ac.kr>
2024-12-11 18:47:30 +0900

    [level 2] Title: 광물 캐기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,41 +1,32 @@
-import itertools
-from collections import deque
-
-pick_name = ["diamond", "iron", "stone"]
-vocab = {name: index for index, name in enumerate(pick_name)}
-vocab["diamond"] = 0
-firo = [[1, 1, 1], [5, 1, 1], [25, 5, 1]]
+mapper = [[1, 1, 1], [5, 1, 1], [25, 5, 1]]
 
 
-def mapper(mine: list):
-    return list(map(mine.count, ["diamond", "iron", "stone"]))
+def getFiro(pick, mineral):
+    x = ["diamond", "iron", "stone"].index(mineral)
+    return mapper[pick][x]
+
+
+def split(minerals):
+    result = []
+    for i in range(0, len(minerals), 5):
+        result.append(minerals[i : i + 5])
+    return result
+
+
+def getPick(picks):
+    for i in range(3):
+        if picks[i] > 0:
+            picks[i] -= 1
+            return i
 
 
 def solution(picks, minerals):
-    # 광물을 5개씩 묶고 곡괭이 갯수만큼만 가져온다.
-    minerals = [minerals[i:i + 5] for i in range(0, len(minerals), 5)][:sum(picks)]
+    minerals = split(minerals)[: sum(picks)]
+    minerals.sort(key=lambda x: sum([getFiro(2, v) for v in x]), reverse=True)
 
-    # [diamond, iron, stone] 각 갯수만큼으로 map 한다.
-    minerals = list(map(mapper, minerals))
+    sumv = 0
+    for m in minerals:
+        p = getPick(picks)
+        sumv += sum([getFiro(p, mm) for mm in m])
 
-    # diamond, iron, stone 갯수로 정렬한다. (다이아, 철, 돌 많이 가지고 있는거 순)
-    minerals.sort(reverse=True)
-
-    firo_sum = 0
-
-    # 다이아를 먼저 다 쓰고, 철, 돌 곡괭이 순서대로 다 사용한다.
-    for mine in minerals:
-        idx = 0
-        # diamond 사용
-        if picks[0] > 0:
-            picks[0] -= 1
-            idx = 0
-        elif picks[1] > 0:
-            picks[1] -= 1
-            idx = 1
-        elif picks[2] > 0:
-            idx = 2
-
-        firo_sum += sum(firo[idx][index] * m for index, m in enumerate(mine))
-
-    return firo_sum
+    return sumv
