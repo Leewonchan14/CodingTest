1f5566c0b5aaf215f3623bb1e0ed4449cd56350f
이원찬 <twoone14@tukorea.ac.kr>
2024-12-11 15:14:44 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 346.13 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,36 +1,40 @@
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+
+def lotate(y1, x1, y2, x2, maps):
+    minv = float("inf")
+
+    x1 -= 1
+    x2 -= 1
+    y1 -= 1
+    y2 -= 1
+    y, x = y1, x1
+    temp = maps[y][x]
+    for i in range(4):
+        while True:
+            minv = min(minv, maps[y][x])
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if ny < y1 or ny > y2 or nx < x1 or nx > x2:
+                break
+
+            temp, maps[ny][nx] = maps[ny][nx], temp
+            y = ny
+            x = nx
+
+    return minv
+
+
+def makeMap(rows, columns):
+    return [[r * columns + c + 1 for c in range(columns)] for r in range(rows)]
+
+
 def solution(rows, columns, queries):
-    nm = [[0] * (columns + 1) for _ in range(rows + 1)]
-    for i in range(1, rows + 1):
-        for j in range(1, columns + 1):
-            nm[i][j] = (i - 1) * columns + j
+    maps = makeMap(rows, columns)
+    result = []
+    for query in queries:
+        result.append(lotate(*query, maps))
 
-    rs = []
-
-    for y1, x1, y2, x2 in queries:
-        nx = x1
-        ny = y1
-
-        temp = nm[ny][nx]
-
-        minv = temp
-
-        for nx in range(x1 + 1, x2 + 1):
-            nm[ny][nx], temp = temp, nm[ny][nx]
-            minv = min(nm[ny][nx], minv)
-
-        for ny in range(y1 + 1, y2 + 1):
-            nm[ny][nx], temp = temp, nm[ny][nx]
-            minv = min(nm[ny][nx], minv)
-
-        for nx in range(x2 - 1, x1 - 1, -1):
-            nm[ny][nx], temp = temp, nm[ny][nx]
-            minv = min(nm[ny][nx], minv)
-
-        for ny in range(y2 - 1, y1 - 1, -1):
-            nm[ny][nx], temp = temp, nm[ny][nx]
-            minv = min(nm[ny][nx], minv)
-
-        rs.append(minv)
-
-    return rs
-
+    return result
