9a27ca295c7b387f0099cf10dc7f8a8e0177c1b7
이원찬 <twoone14@tukorea.ac.kr>
2024-12-11 15:27:11 +0900

    [level 2] Title: 리코쳇 로봇, Time: 17.77 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,50 +1,74 @@
 from collections import deque
-def get_direction(y,x,board):
-    dy = [0,1,0,-1]
-    dx = [1,0,-1,0]
-    ls = []
-    for i in range(4):
-        y1 = y
-        x1 = x
-        while True:
-            ny = y1 + dy[i]
-            nx = x1 + dx[i]
-            if 0 <= ny < len(board) and 0 <= nx < len(board[0]) and "D" != board[ny][nx]:
-                pass
-            else:
-                break
-            y1 = ny
-            x1 = nx
-            
-        ls.append((y1,x1))
-        
-    return ls
-            
-            
-    
+
+dy = [0, -1, 0, 1]
+dx = [-1, 0, 1, 0]
+
+
+def goWhile(point, board, i):
+    y, x = point
+    while True:
+        ny = y + dy[i]
+        nx = x + dx[i]
+
+        if not (
+            0 <= ny < len(board) and 0 <= nx < len(board[0]) and board[ny][nx] != "D"
+        ):
+            break
+
+        y = ny
+        x = nx
+
+    return y, x
+
+
+def bfs(board, start, end, visited):
+    sy, sx = start
+    ey, ex = end
+
+    que = deque([])
+    que.append((sy, sx, 0))
+    visited.add((sy, sx))
+
+    while que:
+        y, x, cnt = que.popleft()
+        if end[0] == y and end[1] == x:
+            return cnt
+
+        for i in range(4):
+            ny, nx = goWhile((y, x), board, i)
+            if (ny, nx) in visited:
+                continue
+
+            visited.add((ny, nx))
+            que.append((ny, nx, cnt + 1))
+
+    return -1
+
+
+def toMap(board):
+    return [list(b) for b in board]
+
+
+def getPoint(board):
+    start = None
+    end = None
+
+    for r, r_ in enumerate(board):
+        for c, c_ in enumerate(r_):
+            if c_ == "R":
+                start = (r, c)
+
+            if c_ == "G":
+                end = (r, c)
+
+    return start, end
+
 
 def solution(board):
-    start = (0,0)
-    for r in range(len(board)):
-        for c in range(len(board[0])):
-            if board[r][c] == "R":
-                start = (r,c)
-    
-    visited = [[False] * len(board[0]) for _ in range(len(board))]
-    que = deque()
-    que.append((start[0],start[1], 0))
-    
-    while que:
-        y,x, cnt = que.popleft()
-        if board[y][x] == "G":
-            return cnt
-                
-        for ny,nx in get_direction(y,x,board):
-            if not visited[ny][nx]:
-                visited[ny][nx] = True
-                que.append((ny,nx,cnt + 1))
-                    
-    return -1
-            
-        
-    
+    board = toMap(board)
+    start, end = getPoint(board)
+    visited = set()
+    return bfs(board, start, end, visited)
+
+
+# solution(["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."])
