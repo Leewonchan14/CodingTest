ba35523f5e49554582a7f09f0ff4bdf06a6833e6
이원찬 <twoone14@tukorea.ac.kr>
2024-12-11 15:51:27 +0900

    [level 2] Title: 테이블 해시 함수, Time: 112.85 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,22 +1,43 @@
-def solution(data, col, row_begin, row_end):
-    data.sort(key=lambda d: (d[col - 1], -d[0]))
-    
-    data = data[row_begin - 1:row_end]
-    
-    S_I = [sum(c % (i+row_begin) for c in row) for i, row in enumerate(data)]
-    
-    first = -1
-    
-    for i, s_i in enumerate(S_I) :
-        if i == 0:
-            first = s_i
-            continue
+def sortKey(x, col):
+    return (x[col - 1], 1 / x[0])
+
+
+def bitwise(a, b):
+    al = list(bin(a)[2:])
+    bl = list(bin(b)[2:])
+    result = [""] * max(len(al), len(bl))
+    i = -1
+    while i >= -max(len(al), len(bl)):
+        if i >= -min(len(al), len(bl)):
+            if al[i] != bl[i]:
+                result[i] = "1"
+            else:
+                result[i] = "0"
+        else:
+            result[i] = al[i] if i >= -len(al) else bl[i]
+
+        i -= 1
+
+    return int("".join(result), 2)
+
+def getMod(row_begin, row_end, data):
+    result = []
+    for i in range(row_begin - 1, row_end):
+        key = list(map(lambda x: x % (i + 1), data[i]))
+        result.append(sum(key)) 
         
-        first = first ^ s_i
+    return result
     
-    return first
-    
-    
-    
-    
-    
+
+
+def solution(data, col, row_begin, row_end):
+    data.sort(key=lambda x: sortKey(x, col))
+    li = getMod(row_begin, row_end, data)
+    result = li[0]
+    for i in range(1, len(li)):
+        result = bitwise(result, li[i])
+        
+        
+    return result
+     
+  
