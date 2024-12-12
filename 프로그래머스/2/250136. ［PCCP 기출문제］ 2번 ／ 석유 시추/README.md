f1e894cf349f4e9e9a097750d40c99ab5b0c2c6f
이원찬 <twoone14@tukorea.ac.kr>
2024-12-12 12:59:03 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 520.28 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=python3) 
 
 ### 성능 요약
 
-메모리: 63.6 MB, 시간: 30.75 ms
+메모리: 13.8 MB, 시간: 520.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 13:15:12
+2024년 12월 12일 12:59:00
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,42 +1,81 @@
 from collections import deque
-def bfs(y,x,visited, land):
-    que = deque()
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+
+def isGood(y, x, land, visited):
+    return (
+        0 <= y < len(land)
+        and 0 <= x < len(land[0])
+        and not visited[y][x]
+        and land[y][x] == 1
+    )
+
+
+def dfs(land, visited, start, c_s):
+    cnt = 1
+    y, x = start
+    c_s.add((x))
     visited[y][x] = True
-    count = 0
-    
-    que.append((y,x,count))
-    
-    dy = [0,-1,0,1]
-    dx = [1,0,-1,0]
-    
-    c_l = [500, -1]
-    
+
+    if x in c_s:
+        pass
+    else:
+        land[y][x] = 0
+
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if not isGood(ny, nx, land, visited):
+            continue
+        key, c_s = dfs(land, visited, (ny, nx), c_s)
+        cnt += key
+
+    return cnt, c_s
+
+
+def bfs(land, visited, start, c_s):
+    y, x = start
+
+    visited[y][x] = True
+    que = deque([(y, x)])
+    total = 0
+    ls = []
+
     while que:
-        y,x,cnt = que.popleft()
-        c_l[0] = min(x, c_l[0])
-        c_l[1] = max(x, c_l[1])
-        count += 1
+        y, x = que.popleft()
+        total += 1
+        if x in c_s:
+            land[y][x] = 0
+        else:
+            c_s.add(x)
+            ls.append((y, x))
+
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0 <= ny < len(land) and 0 <= nx < len(land[0]):
-                if land[ny][nx] == 1 and not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    que.append((ny,nx, cnt + 1))
-    
-    return count, c_l
+            if not isGood(ny, nx, land, visited):
+                continue
+
+            visited[ny][nx] = True
+            que.append((ny, nx))
+
+    for y, x in ls:
+        land[y][x] = total
+
 
 def solution(land):
-    ls = []
     visited = [[False] * len(land[0]) for _ in range(len(land))]
-    for c in range(len(land[0])):
-        for r in range(len(land)):
-            if land[r][c] == 1 and not visited[r][c]:
-                ls.append(bfs(r,c,visited, land))
-        
-    mm = [0] * len(land[0])
-    for cnt, [start, end] in ls:
-        for i in range(start, end + 1):
-            mm[i] += cnt
-    
-    return max(mm)
+
+    for r in range(len(land)):
+        for c in range(len(land[0])):
+            if isGood(r, c, land, visited):
+                bfs(land, visited, (r, c), set())
+
+    maxv = 0
+    for i in range(len(land[0])):
+        maxv = max(sum([land[j][i] for j in range(len(land))]), maxv)
+
+    return maxv
+
