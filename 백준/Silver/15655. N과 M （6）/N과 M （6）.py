71b4065c4b11f106b14659dc1372c9f0920e107f
이원찬 <twoone14@tukorea.ac.kr>
2024-12-16 20:54:12 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,21 @@
+def recur(arr, k, li):
+    if len(li) == k:
+        print(" ".join([str(arr[i]) for i in li]))
+        return
+    
+    
+    for i, a in enumerate(arr):
+        if not li or i > li[-1]:
+            li.append(i)
+            recur(arr, k, li)
+            li.pop()
+
+def main(arr, k):
+    recur(arr, k, [])
+    
+_, k = map(int, input().split())
+
+arr = [int(i) for i in input().split()]
+arr.sort()
+
+main(arr, k)
