63fa8ac42380b328bef9c1c32f2319ff17312e21
이원찬 <twoone14@tukorea.ac.kr>
2024-12-17 13:16:41 +0900

    [Silver I] Title: 스타트와 링크, Time: 2724 ms, Memory: 60620 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,38 +1,48 @@
 import sys
 
-sys.setrecursionlimit(10 ** 9)
-
 input = sys.stdin.readline
 
-n = int(input())
 
-data = [list(map(int, input().split())) for _ in range(n)]
-
-result = []
-
-
-def recursive(team_a, n, point):
-    if len(team_a) == n // 2:
-        team_b = [i for i in range(n) if i not in team_a]
-
-        sumv = 0
-        for i in range(n // 2):
-            for j in range(i + 1, n // 2):
-                sumv += data[team_b[i]][team_b[j]]
-                sumv += data[team_b[j]][team_b[i]]
-
-        result.append((point, sumv))
-        return
+def getMid(n, li, result):
+    if len(li) == n // 2:
+        result.append(li[:])
+        return result
 
     for i in range(n):
-        if not team_a or i > team_a[-1]:
-            team_a.append(i)
-            recursive(team_a, n, sum([data[i][j] for j in team_a]) + sum([data[j][i] for j in team_a]) + point)
-            team_a.pop()
+        if not li or i > li[-1]:
+            li.append(i)
+            result = getMid(n, li, result)
+            li.pop()
+
+    return result
 
 
-recursive([], n, 0)
+def get2(n, li, result):
+    if len(li) == 2:
+        result.append(li[:])
+        return result
 
-result.sort(key=lambda x: abs(x[0] - x[1]))
+    for i in range(n):
+        li.append(i)
+        result = get2(n, li, result)
+        li.pop()
 
-print(abs(result[0][0] - result[0][1]))
+    return result
+
+
+def main(n, arr):
+    minv = float("inf")
+    two = get2(n // 2, [], [])
+    for teamA in getMid(n, [], []):
+        teamB = [i for i in range(n) if i not in teamA]
+        sumA = sum([arr[teamA[a]][teamA[b]] for a, b in two])
+        sumB = sum([arr[teamB[a]][teamB[b]] for a, b in two])
+        minv = min(minv, abs(sumA - sumB))
+
+    return minv
+
+
+n = int(input())
+arr = [[int(i) for i in input().split()] for _ in range(n)]
+
+print(main(n, arr))
