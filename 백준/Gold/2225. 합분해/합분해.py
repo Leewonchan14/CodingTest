09e5e60c1a8a31edbbafd21512eacbbea2c3511e
이원찬 <twoone14@tukorea.ac.kr>
2024-12-20 16:37:17 +0900

    [Gold V] Title: 합분해, Time: 44 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,16 +1,15 @@
 def main(n, k):
-    dp = [[0] * (n + 1) for _ in range(k + 1)]
-    dp[1] = [1] * (n + 1)
-
-    for kk in range(1, k + 1):
-        for nn in range(n + 1):
-            sumv = dp[kk][nn]
-            for j in range(nn + 1):
-                sumv = (sumv + dp[kk - 1][j]) % 10**9
-            dp[kk][nn] = sumv
-
+    dp = [([1] * (n + 1)) for _ in range(k + 1)]
+    for kk in range(2, k + 1):
+        for nn in range(1, n + 1):
+            dp[kk][nn] = (dp[kk - 1][nn] + dp[kk][nn - 1]) % 1000000000
+            
     return dp[k][n]
 
+import sys
 
-a, b = [int(i) for i in input().split()]
-print(main(a, b))
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+print(main(n, k))
