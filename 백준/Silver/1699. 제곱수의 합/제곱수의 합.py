9d6da82dd15070f56e85dce5548465562a917599
이원찬 <twoone14@tukorea.ac.kr>
2024-12-20 16:22:06 +0900

    [Silver II] Title: 제곱수의 합, Time: 1980 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,16 +1,32 @@
-import math
+import sys
 
-dp = [_ for _ in range(10**5 + 1)]
+input = sys.stdin.readline
 
-for i in range(1, 10**5 + 1):
-    for j in range(1, math.floor(i**0.5) + 1):
-        if dp[i] > dp[i - j * j] + 1:
-            dp[i] = dp[i - j * j] + 1
-
+def preNum(n):
+    li = [0]
+    i = 1
+    while i ** 2 <= n:
+        li.append(i ** 2)
+        i += 1
+        
+    return li
 
 def main(n):
+    nums = preNum(n)
+    dp = [0, 1]
+    for i in range(2, n + 1):
+        if int(i ** 0.5) ** 2 == i:
+            dp.append(1)
+            continue
+        dp.append(min([dp[i - j] + 1 for j in nums if j < i and j != 0]))
+        
     return dp[n]
+        
+    
 
+        
+    
+    
 
 n = int(input())
 print(main(n))
