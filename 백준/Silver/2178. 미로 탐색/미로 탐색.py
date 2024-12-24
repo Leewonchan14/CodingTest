aaad91792e6e1565e90402cade131bc1fb26f9bf
이원찬 <twoone14@tukorea.ac.kr>
2024-12-24 14:50:13 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,47 +1,41 @@
-"""
-bfs 로 가장 먼저 도착 했을때 값을 리턴
-def bfs(v)
-v : (y,x)
-
-visited = bool[][]
-
-Queue[(y,x),cnt]
-cnt : int
-"""
-
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 
-def bfs(n, m, v, maped):
-    visited = [[False] * m for _ in range(n)]
-    dy = [0, 1, 0, -1]
-    dx = [1, 0, -1, 0]
-    y, x = v
+dy = [0, -1, 0, 1]
+dx = [1, 0, -1, 0]
+
+def main(maps):
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
     que = deque()
-    visited[y][x] = True
-    que.append((y, x, 1))
-
+    visited[0][0] = True
+    que.append((0, 0, 1))
+    
     while que:
         y, x, cnt = que.popleft()
-
-        if y + 1 == n and x + 1 == m:
+        
+        if y == len(maps) - 1 and x == len(maps[0]) - 1:
             return cnt
-
+        
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
+            
+            if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]) and maps[ny][nx] == "1" and not visited[ny][nx]:
+                visited[ny][nx] = True
+                que.append((ny, nx, cnt + 1))
+                
+    
+    return -1
+    
 
-            if 0 <= ny < n and 0 <= nx < m:
-                if not visited[ny][nx] and maped[ny][nx] == 1:
-                    visited[ny][nx] = True
-                    que.append((ny, nx, cnt + 1))
+n,m = map(int, input().split())
+maps = []
+for _ in range(n):
+    maps.append(list(input().strip()))
+    
+print(main(maps))
+    
 
-
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-maped = [list(map(int, list(input().split()[0]))) for _ in range(n)]
-
-print(bfs(n, m, (0, 0), maped))
+    
