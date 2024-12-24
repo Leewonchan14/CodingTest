b617953046844bdd3cfaae10d7e75f35de9eb2b3
이원찬 <twoone14@tukorea.ac.kr>
2024-12-24 16:28:25 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 276 ms, Memory: 45032 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-// const [n, k] = [5, 17];
+
 const [n, k] = fs
   .readFileSync("/dev/stdin")
   .toString()
@@ -7,13 +7,63 @@ const [n, k] = fs
   .split(" ")
   .map(Number);
 
+class Deque {
+  head = undefined;
+  tail = undefined;
+  constructor() {}
+
+  unshift(v) {
+    const newNode = { v };
+    if (this.head === undefined) {
+      head = newNode;
+      tail = newNode;
+      return;
+    }
+
+    this.head.pre = newNode;
+    this.head = newNode;
+  }
+  push(v) {
+    const newNode = { v };
+    if (this.tail === undefined) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  shift() {
+    const ret = this.head.v;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = this.head;
+    }
+    return ret;
+  }
+  pop() {
+    const ret = this.tail.v;
+    this.tail = this.tail.pre;
+    if (!this.tail) {
+      this.head = this.tail;
+    }
+    return ret;
+  }
+
+  isEmpty() {
+    return this.head === undefined;
+  }
+}
+
 const main = () => {
   const graph = {};
   const visited = Array(10 ** 5 + 1).fill(false);
   let total = 0;
   visited[n] = true;
-  const que = [[n, 0]];
-  while (que.length > 0) {
+  const que = new Deque();
+  que.push([n, 0]);
+  while (!que.isEmpty()) {
     let [start, cnt] = que.shift();
 
     if (start == k) {
