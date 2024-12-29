68a6a9f8311fd528ed5b8612bfe31d06e865f2b4
이원찬 <twoone14@tukorea.ac.kr>
2024-12-30 00:57:01 +0900

    [Silver I] Title: 부분수열의 합, Time: 1364 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+let [_, arr] = fs.readFileSync('dev/stdin').toString().trim().split("\n");
+arr = arr.split(" ").map(Number);
+
+const main = () => {
+    const dp = Array.from({length : 10 ** 6}).fill(true);
+    const li = []
+    const recur = (sumv) => {
+        if (li.length != 0 && sumv < dp.length) {
+            dp[sumv] = false;
+        }
+        
+        if (li.length === arr.length) return;
+        
+        for (let i = 0; i < arr.length; i++){
+            if(!li.length || i > li.at(-1)){
+                li.push(i)
+                recur(sumv + arr[i]);
+                li.pop()
+            }
+        }
+    }
+    
+    recur(0)
+    
+    for(let i = 1; i < dp.length; i++){
+        if(dp[i]) return i;
+    }
+}
+
+console.log(main())
