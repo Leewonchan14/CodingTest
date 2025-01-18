0217df04d91f0bf6b7aeb1613d7d866d8beba9d4
이원찬 <twoone14@tukorea.ac.kr>
2025-01-18 13:56:56 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 468 ms, Memory: 82380 KB -BaekjoonHub

--- 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
@@ -4,37 +4,13 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-dic = {i: [] for i in range(1, n + 1)}
+dic = {i: set() for i in range(1, n + 1)}
 for _ in range(n - 1):
     a, b = map(int, input().split())
-    dic.setdefault(a, []).append(b)
-    dic.setdefault(b, []).append(a)
+    dic[a].add(b)
+    dic[b].add(a)
 
 arr = [int(i) for i in input().split()]
-
-arr_idx = {v: i for i, v in enumerate(arr)}
-
-for i in range(1, n + 1):
-    dic[i].sort(key=lambda x: arr_idx[x])
-
-
-n_arr = []
-
-
-def bfs(start):
-    que = deque()
-    visited = [False] * (n + 1)
-    que.append(start)
-    visited[start] = True
-    n_arr.append(start)
-
-    while que:
-        x = que.popleft()
-        for nx in dic[x]:
-            if not visited[nx]:
-                que.append(nx)
-                n_arr.append(nx)
-                visited[nx] = True
 
 
 def is_same_arr(arr1, arr2):
@@ -48,11 +24,35 @@ def is_same_arr(arr1, arr2):
     return True
 
 
+def bfs(start):
+    que = deque()
+    cnt = 1
+    visited = [False] * (n + 1)
+    que.append(start)
+    visited[start] = True
+
+    while que:
+        x = que.popleft()
+        not_visited = [i for i in dic[x] if not visited[i]]
+        n_arr = arr[cnt : cnt + len(not_visited)]
+        if n_arr and not all([i in dic[x] for i in n_arr]):
+            return 0
+
+        for nx in n_arr:
+            visited[nx] = True
+            cnt += 1
+            que.append(nx)
+
+    if all([visited[i] for i in range(1, n + 1)]):
+        return 1
+
+    return 0
+
+
 def main():
-    bfs(arr[0])
     if arr[0] != 1:
         return 0
-    return 1 if is_same_arr(arr, n_arr) else 0
+    return bfs(arr[0])
 
 
 print(main())
