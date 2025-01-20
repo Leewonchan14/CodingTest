3974c1dc046a6bb0b4949f1e3d43ca8f40d253bc
이원찬 <twoone14@tukorea.ac.kr>
2025-01-20 15:20:31 +0900

    [Gold IV] Title: 돌 그룹, Time: 2800 ms, Memory: 159476 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -0,0 +1,50 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+visited = set()
+visited.add((a, b, c))
+
+
+def find_group(tu):
+    result = []
+    for i in range(3):
+        for j in range(3):
+            if i == j:
+                continue
+
+            if tu[i] < tu[j]:
+                result.append((i, j))
+
+    return result
+
+
+def main():
+    que = deque()
+    que.append((a, b, c))
+
+    while que:
+        tu = que.popleft()
+        if tu[0] == tu[1] == tu[2]:
+            return 1
+
+        for i, j in find_group(tu):
+            k = 3 - i - j
+            li = [0, 0, 0]
+
+            li[i] = tu[i] * 2
+            li[j] = tu[j] - tu[i]
+            li[k] = tu[k]
+
+            ntu = tuple(li)
+
+            if ntu not in visited:
+                visited.add(ntu)
+                que.append(ntu)
+
+    return 0
+
+
+print(main())
