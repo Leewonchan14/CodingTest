acb1e7c062ccf056dcbbbfb2ccf9ff63bc48bd5b
이원찬 <twoone14@tukorea.ac.kr>
2025-01-27 10:39:25 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string.h>
+
+using namespace std;
+
+vector<int> li;
+int n, m;
+string result;
+int visited[10];
+int arr[10];
+int recur();
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        int input;
+        cin >> input;
+
+        arr[i] = input;
+    }
+
+    sort(arr, &arr[n]);
+
+    recur();
+
+    cout << result;
+}
+
+int recur()
+{
+    if (li.size() == m)
+    {
+        for (auto &&i : li)
+        {
+            cout << arr[i] << " ";
+        }
+
+        cout << "\n";
+
+        return 0;
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (li.empty() || i > li.back())
+        {
+            // visited[i] = 1;
+            li.push_back(i);
+            recur();
+            li.pop_back();
+            // visited[i] = 0;
+        }
+    }
+
+    return 0;
+}
