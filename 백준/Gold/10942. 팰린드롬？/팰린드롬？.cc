f9a0f7ac404faefbf9cf78720bb911f8fcbca8b9
이원찬 <twoone14@tukorea.ac.kr>
2025-01-28 18:09:33 +0900

    [Gold IV] Title: 팰린드롬?, Time: 252 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -4,41 +4,32 @@
 using namespace std;
 
 int n, m, s, e;
-int *maps;
-int **costs;
+vector<int> maps;
+vector<vector<int>> costs;
 
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
 
-  cin >> n;
-  maps = new int[n];
-  costs = new int *[n];
-  for (int i = 0; i < n; i++)
-  {
-    cin >> maps[i];
-    costs[i] = new int[n + 1];
-  }
 
-  for (int e = 1; e <= n; e++)
-  {
-    for (int s = e - 1; s >= 0; s--)
-    {
-      costs[s][e] = maps[s] == maps[e - 1] && (s + 1 >= e - 1 || costs[s + 1][e - 1]);
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> n;
+    maps.resize(n);
+    costs.resize(n);
+    for(int i = 0; i < n; i++ ){
+        cin >> maps[i];
+        costs[i].resize(n + 1);
     }
-  }
-
-  cin >> m;
-  for (int i = 0; i < m; i++)
-  {
-    cin >> s >> e;
-    cout << costs[s - 1][e] << "\n";
-  }
-
-  delete[] maps;
-  for (int i = 0; i < n; i++)
-  {
-    delete[] costs[i];
-  }
+    
+    for(int e = 1; e <= n; e ++ ){
+        for(int s = e - 1; s >= 0; s-- ){
+            costs[s][e] = maps[s] == maps[e - 1] && (s + 1 >= e - 1 || costs[s + 1][e - 1]);
+        }
+    }
+    
+    cin >> m;
+    for(int i = 0; i < m; i++){
+        cin >> s >> e;
+        cout << costs[s - 1][e] << "\n";
+    }
 }
