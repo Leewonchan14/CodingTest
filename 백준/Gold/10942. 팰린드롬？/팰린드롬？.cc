f7b7b61c24021f04ee476fca24b59fe8ba126f0d
이원찬 <twoone14@tukorea.ac.kr>
2025-01-28 18:09:53 +0900

    [Gold IV] Title: 팰린드롬?, Time: 232 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -4,32 +4,35 @@
 using namespace std;
 
 int n, m, s, e;
-vector<int> maps;
-vector<vector<int>> costs;
+int *maps;
+int **costs;
 
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
+  cin >> n;
+  maps = new int[n];
+  costs = new int *[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> maps[i];
+    costs[i] = new int[n + 1];
+  }
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    cin >> n;
-    maps.resize(n);
-    costs.resize(n);
-    for(int i = 0; i < n; i++ ){
-        cin >> maps[i];
-        costs[i].resize(n + 1);
+  for (int e = 1; e <= n; e++)
+  {
+    for (int s = e - 1; s >= 0; s--)
+    {
+      costs[s][e] = maps[s] == maps[e - 1] && (s + 1 >= e - 1 || costs[s + 1][e - 1]);
     }
-    
-    for(int e = 1; e <= n; e ++ ){
-        for(int s = e - 1; s >= 0; s-- ){
-            costs[s][e] = maps[s] == maps[e - 1] && (s + 1 >= e - 1 || costs[s + 1][e - 1]);
-        }
-    }
-    
-    cin >> m;
-    for(int i = 0; i < m; i++){
-        cin >> s >> e;
-        cout << costs[s - 1][e] << "\n";
-    }
+  }
+
+  cin >> m;
+  for (int i = 0; i < m; i++)
+  {
+    cin >> s >> e;
+    cout << costs[s - 1][e] << "\n";
+  }
 }
