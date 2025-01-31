bef23f1a76e99d28638f2a1c8c0b8308e5aad67b
이원찬 <twoone14@tukorea.ac.kr>
2025-01-31 12:13:34 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n,m,v;
+vector<int> arr;
+
+int main(){
+    cin >> n >> m;
+    
+    for(int i = 0; i < n; i ++ ){
+        cin >> v;
+        arr.push_back(v);
+    }
+    
+    long sum = arr[0];
+    int l = 0;
+    int r = 1;
+    int cnt = 0;
+    
+    
+    while(true){
+        if(sum == m) {
+            cnt += 1;
+            sum -= arr[l];
+            l += 1;
+            continue;
+        }
+        
+        if (sum < m) {
+            if (r >= n) break;
+            
+            sum += arr[r];
+            r += 1;
+            continue;
+        }
+        if(sum > m) {
+            sum -= arr[l];
+            l += 1;
+            continue;
+        }
+    }
+    
+    cout << cnt << "\n";
+    
+    return 0;
+}
