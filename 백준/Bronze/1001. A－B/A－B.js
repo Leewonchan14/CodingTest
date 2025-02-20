83e61fe1269245ad475aac54f48f11f062b0181b
이원찬 <twoone14@tukorea.ac.kr>
2025-02-20 22:41:28 +0900

    [Bronze V] Title: A-B, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,6 @@
+// 입력을 받아오기 위해 아래 두줄의 코드를 사용하세요. (수정 금지)
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim();
+
+const [a, b] = stdin.split(" ").map(Number);
+console.log(a - b)
