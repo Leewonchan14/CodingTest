c774dcbbaf4277556e7cfdf6a2a1f7dd717ffed9
이원찬 <twoone14@tukorea.ac.kr>
2025-02-23 12:19:48 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -5,11 +5,9 @@ input = sys.stdin.readline
 n = int(input())
 
 def main():
-    def recur(n):
-        if n == 0:
-            return 1
-        return recur(n - 1) * n
-    
-    print(recur(n))
+    mulv = 1
+    for i in range(1, n + 1):
+        mulv *= i
+    print(mulv)
 
 main()
