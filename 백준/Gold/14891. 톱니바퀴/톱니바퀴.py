4d1585af808d82d37d363962e2214480b2ff3f36
lee-won-chan <twoone14@tukorea.ac.kr>
2025-05-04 16:49:10 +0900

    [Gold V] Title: 톱니바퀴, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,35 +1,36 @@
 tops = [["N" if i == "0" else "S" for i in input().strip()] for _ in range(4)]
 n = int(input())
 
-options = [list(map(int, input().split())) for _ in range(n)]
-options = [[i[0] - 1, ("LEFT" if i[1] == -1 else "RIGHT")] for i in options]
-# [[k1, "LEFT"], [k2, "RIGHT"], ...]
+options = [[int(i) for i in input().split()] for _ in range(n)]
+options = [[i[0] - 1, "LEFT" if i[1] == -1 else "RIGHT"] for i in options]
 
-RIGHT_IDX = 2
 LEFT_IDX = 6
+RIGHT_IDX = 2
 
 def spin(k, direct, source):
-    if k < 0 or k >= 4:
-        return
-    
-    top_right = tops[k][RIGHT_IDX]
     top_left = tops[k][LEFT_IDX]
+    top_right = tops[k][RIGHT_IDX]
     
-    other_direct = "RIGHT" if direct == "LEFT" else "LEFT"
+    other_direct = "LEFT" if direct == "RIGHT" else "RIGHT"
+    
     if direct == "LEFT":
         tops[k] = tops[k][1:] + [tops[k][0]]
     else:
         tops[k] = [tops[k][-1]] + tops[k][:-1]
         
-    
-    if k - 1 >= 0 and k - 1 != source and top_left != tops[k - 1][RIGHT_IDX]:
+    if k - 1 != source and k - 1 >= 0 and top_left != tops[k - 1][RIGHT_IDX]:
         spin(k - 1, other_direct, k)
         
-    if k + 1 < 4 and k + 1 != source and top_right != tops[k + 1][LEFT_IDX]:
+    if k + 1 != source and k + 1 <= 3 and top_right != tops[k + 1][LEFT_IDX]:
         spin(k + 1, other_direct, k)
+        
+        
+for k, direct in options:
+    spin(k, direct, -1)
 
-for kk, dd in options:
-    spin(kk, dd, -1)
+t12 = [t[0] for t in tops]
+
+bit_t12 = ["1" if t == "S" else "0" for t in t12]
+
+print(int("".join(bit_t12[::-1]), 2))
     
-print(int("".join(["1" if t[0] == "S" else "0" for t in tops[::-1]]), 2))
-
