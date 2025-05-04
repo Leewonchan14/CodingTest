1ec525b988f2616c376204c5bc2d7fc71422cf77
lee-won-chan <twoone14@tukorea.ac.kr>
2025-05-04 15:47:58 +0900

    [Gold V] Title: 크리보드, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.py
+++ 백준/Gold/11058. 크리보드/크리보드.py
@@ -1,37 +1,23 @@
-buffer = 0
-cnt = 0
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
 
-# 처음에 그리디 문제 인줄
-dp = [0] * 101
-
-for i in range(7):
-    dp[i] = i
+dp = [0, 1, 2, 3, 4, 5, 6]
 
 
-def fun(k):
-    if k == 0 or dp[k] != 0:
-        return dp[k]
+def main():  
+    while len(dp) <= n:
+        # 1. A한번 더쓰기
+        v1 = dp[-1] + 1
+        # 2. 3단계 전 복붙
+        v2 = dp[-3] * 2
+        # 3. 4단계 전 복붙
+        v3 = dp[-4] * 3
+        # 4. 5단계 전의 버퍼 복붙
+        v4 = dp[-5] * 4
+        # 5. 6단계 전의 버퍼 복붙
+        v5 = dp[-6] * 5
+        
+        dp.append(max([v1, v2, v3, v4, v5]))
+        
+    return dp[n]
 
-    # 1. A 만 붙히는거
-    a = fun(k - 1) + 1
-
-    # 2. 1복붙
-    b = fun(k - 3) * 2
-
-    # 3. 2복붙
-    c = fun(k - 4) * 3
-
-    # 4. 3복붙
-    d = fun(k - 5) * 4
-
-    dp[k] = max(a, b, c, d)
-
-    return dp[k]
-
-
-print(fun(n))
+print(main())
