27a94770c3ab62c2fb9fa477acdd6fa5e4f87f65
lee-won-chan <twoone14@tukorea.ac.kr>
2025-05-18 15:57:08 +0900

    [Silver III] Title: N과 M (1), Time: 1648 ms, Memory: 13676 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,30 +1,21 @@
-const fs = require("fs");
+const fs = require('fs');
+const [n, k] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
 
-// const [n, m] = [4,2]
-
-const [n, m] = fs
-.readFileSync("/dev/stdin")
-.toString()
-.trim()
-.split(" ")
-.map(Number);
-
-const visited = {}
-
-const recursive = (li) => { 
-  if(li.length === m){
-    console.log(li.join(" "));
-    return;
-  }
-
-  for(let i = 1; i<= n; i++){
-    if(visited[i]) continue;
-    visited[i] = true;
-    li.push(i);
-    recursive(li);
-    li.pop();
-    visited[i] = false;
-  }
+const li = []
+const recur = () => {
+    if (li.length == k){
+        console.log(li.join(" "));
+        return
+    }
+        
+    
+    for(let i = 1; i <= n; i++){
+        if (li.includes(i)) continue;
+        
+        li.push(i)
+        recur()
+        li.pop()
+    }    
 }
 
-recursive([]);
+recur();
